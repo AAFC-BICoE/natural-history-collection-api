c97bc6b8e17170edd70161674188518daae51cb3
Christian Gendreau <christiangendreau@gmail.com>
2020-10-30 14:31:57 -0400

    Support #20355 Implement initial specs of Collecting Event Added inital set of fields to entityt and database schema

---
+++ src/main/java/ca/gc/aafc/collection/api/entities/CollectingEvent.java
@@ -0,0 +1,65 @@
+package ca.gc.aafc.collection.api.entities;
+
+import ca.gc.aafc.dina.entity.DinaEntity;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.NaturalId;
+import org.hibernate.annotations.NaturalIdCache;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@RequiredArgsConstructor
+@SuppressFBWarnings(justification = "ok for Hibernate Entity", value = { "EI_EXPOSE_REP", "EI_EXPOSE_REP2" })
+@NaturalIdCache
+public class CollectingEvent implements DinaEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @NaturalId
+  @NotNull
+  @Column(unique = true)
+  private UUID uuid;
+
+  // Might not be the final choice to store lat/long
+  private Double decimalLatitude;
+  private Double decimalLongitude;
+
+  private Integer coordinateUncertaintyInMeters;
+  private String verbatimCoordinates;
+
+  private LocalDateTime startEventDateTime;
+  private Short startEventDateTimePrecision;
+
+  private LocalDateTime endEventDateTime;
+  private Short endEventDateTimePrecision;
+
+  private String verbatimEventDateTime;
+
+  @Column(insertable = false, updatable = false)
+  private OffsetDateTime createdOn;
+
+  @NotBlank
+  @Column(updatable = false)
+  private String createdBy;
+
+}
--- src/test/java/ca/gc/aafc/collection/api/CollectionModuleBaseIT.java
+++ src/test/java/ca/gc/aafc/collection/api/CollectionModuleBaseIT.java
@@ -1,22 +1,16 @@
 package ca.gc.aafc.collection.api;
 
 import ca.gc.aafc.dina.testsupport.PostgresTestContainerInitializer;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 
 import javax.transaction.Transactional;
 
-@SpringBootTest(
-  classes = CollectionModuleApiLauncher.class,
-  webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
-)
-public class CollectionModuleStartupTest extends CollectionModuleBaseIT {
-
-  @Test
-  public void dummyTest() {
-    //empty test
-  }
-
+@SpringBootTest(classes = CollectionModuleApiLauncher.class)
+@TestPropertySource(properties = "spring.config.additional-location=classpath:application-test.yml")
+@Transactional
+@ContextConfiguration(initializers = { PostgresTestContainerInitializer.class })
+public class CollectionModuleBaseIT {
 }
+
