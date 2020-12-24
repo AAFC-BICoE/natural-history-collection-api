de6728168921573d0bc65f94fb2c857e3c484bea
xuemeigan <xuemei.gan@canada.ca>
2020-12-24 13:41:39 -0500

    Support 21234 Add support for group of collector - Fix check style

--- src/main/java/ca/gc/aafc/collection/api/entities/CollectorGroup.java
+++ src/main/java/ca/gc/aafc/collection/api/entities/CollectorGroup.java
@@ -18,13 +18,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-
 import com.vladmihalcea.hibernate.type.array.UUIDArrayType;
-
 import java.time.OffsetDateTime;
-import java.util.LinkedHashSet;
 import java.util.UUID;
 
 @Entity
@@ -54,7 +50,7 @@ public class CollectorGroup implements DinaEntity {
   @Column(updatable = false)
   private String createdBy;
 
-  @NotNull
+  @NotBlank
   @Column(unique = true)
   private String name;  
 
