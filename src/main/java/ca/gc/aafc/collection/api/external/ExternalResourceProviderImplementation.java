17ca442f02f636607dd019c7e8f5b5ca9fd23d52
Jkeyuk <jonkero2@gmail.com>
2021-01-11 07:32:42 -0500

    21368 attach doc to collecting event
    checkstyle fix

--- src/main/java/ca/gc/aafc/collection/api/external/ExternalResourceProviderImplementation.java
+++ src/main/java/ca/gc/aafc/collection/api/external/ExternalResourceProviderImplementation.java
@@ -9,17 +9,17 @@ import java.util.Set;
 @Component
 public class ExternalResourceProviderImplementation implements ExternalResourceProvider {
 
-  public static final Map<String, String> typeToReferenceMap = Map.of(
+  public static final Map<String, String> TYPE_TO_REFERENCE_MAP = Map.of(
     "metadata", "objectstore/api/v1/metadata"
   );
 
   @Override
   public String getReferenceForType(String type) {
-    return typeToReferenceMap.get(type);
+    return TYPE_TO_REFERENCE_MAP.get(type);
   }
 
   @Override
   public Set<String> getTypes() {
-    return typeToReferenceMap.keySet();
+    return TYPE_TO_REFERENCE_MAP.keySet();
   }
 }
