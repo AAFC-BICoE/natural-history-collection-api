c47a4f2d40456e97604d3a2a29845707b11c6a42
Jkeyuk <jonkero2@gmail.com>
2021-01-11 09:32:09 -0500

    21374 add group to collecting event
    resolve merge conflicts

--- src/test/java/ca/gc/aafc/collection/api/repository/CollectingEventRepositoryIT.java
+++ src/test/java/ca/gc/aafc/collection/api/repository/CollectingEventRepositoryIT.java
@@ -47,29 +47,11 @@ public class CollectingEventRepositoryIT extends CollectionModuleBaseIT {
   private static final LocalDate endDate = LocalDate.of(2002, 10, 10);
   private static final LocalTime endTime = LocalTime.of(10, 10);
 
-  private CollectingEvent createTestCollectingEvent() {
-    testCollectingEvent = CollectingEventFactory.newCollectingEvent()
-      .startEventDateTime(LocalDateTime.of(startDate, startTime))
-      .startEventDateTimePrecision((byte) 8)
-      .endEventDateTime(LocalDateTime.of(endDate, endTime))
-      .endEventDateTimePrecision((byte) 8)
-      .verbatimEventDateTime("XI-02-1798")
-      .decimalLatitude(26.089)
-      .decimalLongitude(106.36)
-      .coordinateUncertaintyInMeters(208)
-      .verbatimCoordinates("26.089, 106.36")
-      .verbatimCollector("Jon and the gang")
-      .collectors(List.of(UUID.randomUUID()))
-      .build();
-
-    service.save(testCollectingEvent);
-    return testCollectingEvent;
-  }
-
   @BeforeEach
   public void setup() {
     createTestCollectingEvent();
   }
+
   private CollectingEvent createTestCollectingEvent() {
     testCollectingEvent = CollectingEventFactory.newCollectingEvent()
       .startEventDateTime(LocalDateTime.of(startDate, startTime))
@@ -82,57 +64,11 @@ public class CollectingEventRepositoryIT extends CollectionModuleBaseIT {
       .coordinateUncertaintyInMeters(208)
       .verbatimCoordinates("26.089, 106.36")
       .attachment(List.of(UUID.randomUUID()))
+      .collectors(List.of(UUID.randomUUID()))
       .build();
 
     service.save(testCollectingEvent);
     return testCollectingEvent;
-  }
-
-  @BeforeEach
-  public void setup() {
-    createTestCollectingEvent();
-  }
-
-  @Test
-  public void findCollectingEvent_whenNoFieldsAreSelected_CollectingEventReturnedWithAllFields() {
-    CollectingEventDto collectingEventDto = collectingEventRepository
-      .findOne(testCollectingEvent.getUuid(), new QuerySpec(CollectingEventDto.class));
-    assertNotNull(collectingEventDto);
-    assertEquals(testCollectingEvent.getUuid(), collectingEventDto.getUuid());
-    assertEquals(testCollectingEvent.getCreatedBy(), collectingEventDto.getCreatedBy());
-    assertEquals(
-      testCollectingEvent.supplyStartISOEventDateTime().toString(),
-      collectingEventDto.getStartEventDateTime());
-    assertEquals(
-      testCollectingEvent.supplyEndISOEventDateTime().toString(),
-      collectingEventDto.getEndEventDateTime());
-    assertEquals("XI-02-1798", collectingEventDto.getVerbatimEventDateTime());
-    assertEquals(26.089, collectingEventDto.getDecimalLatitude());
-    assertEquals(106.36, collectingEventDto.getDecimalLongitude());
-    assertEquals(208, collectingEventDto.getCoordinateUncertaintyInMeters());
-    assertEquals("26.089, 106.36", collectingEventDto.getVerbatimCoordinates());
-    assertEquals("Jon and the gang", collectingEventDto.getVerbatimCollector());
-    assertEquals(
-      testCollectingEvent.getCollectors().get(0).toString(),
-      collectingEventDto.getCollectors().get(0).getId());
-  }
-
-  @Test
-  public void create_WithAuthenticatedUser_SetsCreatedBy() {
-    CollectingEventDto ce = new CollectingEventDto();
-    ce.setUuid(UUID.randomUUID());
-    ce.setStartEventDateTime(ISODateTime.parse("2007-12-03T10:15:30").toString());
-    ce.setEndEventDateTime(ISODateTime.parse("2007-12-04T11:20:20").toString());
-    ce.setVerbatimCoordinates("26.089, 106.36");
-    ce.setVerbatimCollector("Jon and the gang");
-    ce.setCollectors(List.of(ExternalRelationDto.builder()
-      .type("agent")
-      .id(UUID.randomUUID().toString()).build()));
-    CollectingEventDto result = collectingEventRepository.findOne(
-      collectingEventRepository.create(ce).getUuid(),
-      new QuerySpec(CollectingEventDto.class));
-    assertNotNull(result.getCreatedBy());
-    assertEquals(ce.getCollectors().get(0).getId(), result.getCollectors().get(0).getId());
   }
 
   @Test
@@ -156,6 +92,9 @@ public class CollectingEventRepositoryIT extends CollectionModuleBaseIT {
     assertEquals(
       testCollectingEvent.getAttachment().get(0).toString(),
       collectingEventDto.getAttachment().get(0).getId());
+    assertEquals(
+      testCollectingEvent.getCollectors().get(0).toString(),
+      collectingEventDto.getCollectors().get(0).getId());
   }
 
   @Test
@@ -167,11 +106,14 @@ public class CollectingEventRepositoryIT extends CollectionModuleBaseIT {
     ce.setVerbatimCoordinates("26.089, 106.36");
     ce.setAttachment(List.of(
       ExternalRelationDto.builder().id(UUID.randomUUID().toString()).type("file").build()));
+    ce.setCollectors(
+      List.of(ExternalRelationDto.builder().type("agent").id(UUID.randomUUID().toString()).build()));
     CollectingEventDto result = collectingEventRepository.findOne(
       collectingEventRepository.create(ce).getUuid(),
       new QuerySpec(CollectingEventDto.class));
     assertNotNull(result.getCreatedBy());
     assertEquals(ce.getAttachment().get(0).getId(), result.getAttachment().get(0).getId());
+    assertEquals(ce.getCollectors().get(0).getId(), result.getCollectors().get(0).getId());
   }
 
 }
