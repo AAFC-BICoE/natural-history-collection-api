6f72ecf58596a93bbc4c06b01544e61ab0f12fea
Jkeyuk <jonkero2@gmail.com>
2021-01-11 08:56:12 -0500

    21374 add group to collecting event
    fix group factory and update CollectingEventRepositoryIT.java for new relation

--- src/test/java/ca/gc/aafc/collection/api/repository/CollectingEventRepositoryIT.java
+++ src/test/java/ca/gc/aafc/collection/api/repository/CollectingEventRepositoryIT.java
@@ -10,6 +10,9 @@ import java.util.UUID;
 
 import javax.inject.Inject;
 
+import ca.gc.aafc.collection.api.dto.CollectorGroupDto;
+import ca.gc.aafc.collection.api.entities.CollectorGroup;
+import ca.gc.aafc.collection.api.testsupport.factories.CollectorGroupFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -20,67 +23,85 @@ import ca.gc.aafc.collection.api.entities.CollectingEvent;
 import ca.gc.aafc.collection.api.testsupport.factories.CollectingEventFactory;
 import io.crnk.core.queryspec.QuerySpec;
 
-public class CollectingEventRepositoryIT extends CollectionModuleBaseIT{
-    @Inject
-    private CollectingEventRepository collectingEventRepository;
-  
-    private CollectingEvent testCollectingEvent;
+public class CollectingEventRepositoryIT extends CollectionModuleBaseIT {
+  @Inject
+  private CollectingEventRepository collectingEventRepository;
 
-    private static final LocalDate startDate = LocalDate.of(2000,1, 1);
-    private static final LocalTime startTime = LocalTime.of(0,1);
-  
-    private static final LocalDate endDate = LocalDate.of(2002,10, 10);
-    private static final LocalTime endTime = LocalTime.of(10,10);      
+  private CollectingEvent testCollectingEvent;
+  private CollectorGroup groupRelation;
 
-    private CollectingEvent createTestCollectingEvent() {
-      testCollectingEvent = CollectingEventFactory.newCollectingEvent()
-        .startEventDateTime(LocalDateTime.of(startDate, startTime))
-        .startEventDateTimePrecision((byte) 8)
-        .endEventDateTime(LocalDateTime.of(endDate, endTime))
-        .endEventDateTimePrecision((byte) 8)
-        .verbatimEventDateTime("XI-02-1798")
-        .decimalLatitude(26.089)
-        .decimalLongitude(106.36)
-        .coordinateUncertaintyInMeters(208)
-        .verbatimCoordinates("26.089, 106.36")
-        .build();
-  
-      service.save(testCollectingEvent);
-      return testCollectingEvent;
-    }
-  
-    @BeforeEach
-    public void setup(){
-      createTestCollectingEvent();
-    }    
+  private static final LocalDate startDate = LocalDate.of(2000, 1, 1);
+  private static final LocalTime startTime = LocalTime.of(0, 1);
 
-    @Test
-    public void findCollectingEvent_whenNoFieldsAreSelected_CollectingEventReturnedWithAllFields() {
-      CollectingEventDto collectingEventDto = collectingEventRepository
-          .findOne(testCollectingEvent.getUuid(), new QuerySpec(CollectingEventDto.class));
-      assertNotNull(collectingEventDto);
-      assertEquals(testCollectingEvent.getUuid(), collectingEventDto.getUuid());
-      assertEquals(testCollectingEvent.getCreatedBy(), collectingEventDto.getCreatedBy());
-      assertEquals(testCollectingEvent.supplyStartISOEventDateTime().toString(), collectingEventDto.getStartEventDateTime());
-      assertEquals(testCollectingEvent.supplyEndISOEventDateTime().toString(), collectingEventDto.getEndEventDateTime());
-      assertEquals("XI-02-1798", collectingEventDto.getVerbatimEventDateTime());    
-      assertEquals(26.089, collectingEventDto.getDecimalLatitude());    
-      assertEquals(106.36, collectingEventDto.getDecimalLongitude());    
-      assertEquals(208, collectingEventDto.getCoordinateUncertaintyInMeters());    
-      assertEquals("26.089, 106.36", collectingEventDto.getVerbatimCoordinates());    
+  private static final LocalDate endDate = LocalDate.of(2002, 10, 10);
+  private static final LocalTime endTime = LocalTime.of(10, 10);
 
-    }   
-    
-    @Test
-    public void create_WithAuthenticatedUser_SetsCreatedBy() {
-      CollectingEventDto ce = new CollectingEventDto();
-      ce.setUuid(UUID.randomUUID());
-      ce.setStartEventDateTime(ISODateTime.parse("2007-12-03T10:15:30").toString());
-      ce.setEndEventDateTime(ISODateTime.parse("2007-12-04T11:20:20").toString());
-      ce.setVerbatimCoordinates("26.089, 106.36");
-      CollectingEventDto result = collectingEventRepository.findOne(collectingEventRepository.create(ce).getUuid(),
-          new QuerySpec(CollectingEventDto.class));
-      assertNotNull(result.getCreatedBy());
-    }    
-    
+  private CollectingEvent createTestCollectingEvent(CollectorGroup groupRelation) {
+    CollectingEvent newEvent = CollectingEventFactory.newCollectingEvent()
+      .startEventDateTime(LocalDateTime.of(startDate, startTime))
+      .startEventDateTimePrecision((byte) 8)
+      .endEventDateTime(LocalDateTime.of(endDate, endTime))
+      .endEventDateTimePrecision((byte) 8)
+      .verbatimEventDateTime("XI-02-1798")
+      .decimalLatitude(26.089)
+      .decimalLongitude(106.36)
+      .coordinateUncertaintyInMeters(208)
+      .verbatimCoordinates("26.089, 106.36")
+      .collectorGroup(groupRelation)
+      .build();
+
+    service.save(newEvent);
+    return newEvent;
+  }
+
+  @BeforeEach
+  public void setup() {
+    groupRelation = createCollectorGroup();
+    testCollectingEvent = createTestCollectingEvent(groupRelation);
+  }
+
+  private CollectorGroup createCollectorGroup() {
+    CollectorGroup group = CollectorGroupFactory.newCollectorGroup().build();
+    service.save(group);
+    return group;
+  }
+
+  @Test
+  public void findCollectingEvent_whenNoFieldsAreSelected_CollectingEventReturnedWithAllFields() {
+    CollectingEventDto collectingEventDto = collectingEventRepository
+      .findOne(testCollectingEvent.getUuid(), new QuerySpec(CollectingEventDto.class));
+    assertNotNull(collectingEventDto);
+    assertEquals(testCollectingEvent.getUuid(), collectingEventDto.getUuid());
+    assertEquals(testCollectingEvent.getCreatedBy(), collectingEventDto.getCreatedBy());
+    assertEquals(
+      testCollectingEvent.supplyStartISOEventDateTime().toString(),
+      collectingEventDto.getStartEventDateTime());
+    assertEquals(
+      testCollectingEvent.supplyEndISOEventDateTime().toString(),
+      collectingEventDto.getEndEventDateTime());
+    assertEquals("XI-02-1798", collectingEventDto.getVerbatimEventDateTime());
+    assertEquals(26.089, collectingEventDto.getDecimalLatitude());
+    assertEquals(106.36, collectingEventDto.getDecimalLongitude());
+    assertEquals(208, collectingEventDto.getCoordinateUncertaintyInMeters());
+    assertEquals("26.089, 106.36", collectingEventDto.getVerbatimCoordinates());
+    assertEquals(groupRelation.getUuid(), testCollectingEvent.getCollectorGroup().getUuid());
+  }
+
+  @Test
+  public void create_WithAuthenticatedUser_SetsCreatedBy() {
+    CollectorGroupDto groupDto = new CollectorGroupDto();
+    groupDto.setUuid(groupRelation.getUuid());
+    CollectingEventDto ce = new CollectingEventDto();
+    ce.setUuid(UUID.randomUUID());
+    ce.setStartEventDateTime(ISODateTime.parse("2007-12-03T10:15:30").toString());
+    ce.setEndEventDateTime(ISODateTime.parse("2007-12-04T11:20:20").toString());
+    ce.setVerbatimCoordinates("26.089, 106.36");
+    ce.setCollectorGroup(groupDto);
+    CollectingEventDto result = collectingEventRepository.findOne(
+      collectingEventRepository.create(ce).getUuid(),
+      new QuerySpec(CollectingEventDto.class));
+    assertNotNull(result.getCreatedBy());
+    assertEquals(groupRelation.getUuid(), result.getCollectorGroup().getUuid());
+  }
+
 }
--- src/test/java/ca/gc/aafc/collection/api/testsupport/factories/CollectorGroupFactory.java
+++ src/test/java/ca/gc/aafc/collection/api/testsupport/factories/CollectorGroupFactory.java
@@ -2,28 +2,30 @@ package ca.gc.aafc.collection.api.testsupport.factories;
 
 import ca.gc.aafc.collection.api.entities.CollectorGroup;
 import ca.gc.aafc.dina.testsupport.factories.TestableEntityFactory;
+import org.testcontainers.shaded.org.apache.commons.lang.RandomStringUtils;
 
 import java.util.UUID;
 
 public class CollectorGroupFactory implements TestableEntityFactory<CollectorGroup> {
 
-    @Override
-    public CollectorGroup getEntityInstance() {
-      return newCollectorGroup().build();
-    }
-
-    /**
-     * Static method that can be called to return a configured builder that can be
-     * further customized to return the actual entity object, call the .build()
-     * method on a builder.
-     *
-     * @return Pre-configured builder with all mandatory fields set
-     */
-    public static CollectorGroup.CollectorGroupBuilder newCollectorGroup() {
-      return CollectorGroup
-          .builder()
-          .uuid(UUID.randomUUID())
-          .createdBy("test user");
-    }
-
+  @Override
+  public CollectorGroup getEntityInstance() {
+    return newCollectorGroup().build();
   }
+
+  /**
+   * Static method that can be called to return a configured builder that can be further customized to return
+   * the actual entity object, call the .build() method on a builder.
+   *
+   * @return Pre-configured builder with all mandatory fields set
+   */
+  public static CollectorGroup.CollectorGroupBuilder newCollectorGroup() {
+    return CollectorGroup
+      .builder()
+      .name(RandomStringUtils.randomAlphabetic(4))
+      .uuid(UUID.randomUUID())
+      .agentIdentifiers(new UUID[]{UUID.randomUUID()})
+      .createdBy("test user");
+  }
+
+}
