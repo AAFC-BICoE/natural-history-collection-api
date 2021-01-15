e07a45a4a0d80b5d9e8eb08039d5700b839fd905
Jkeyuk <jonkero2@gmail.com>
2021-01-15 11:38:28 -0500

    21382 integration test against api spec
    set up test for collecting event

---
+++ src/test/java/ca/gc/aafc/collection/api/openapi/CollectingEventOpenApiIT.java
@@ -0,0 +1,87 @@
+package ca.gc.aafc.collection.api.openapi;
+
+import ca.gc.aafc.collection.api.CollectionModuleApiLauncher;
+import ca.gc.aafc.collection.api.datetime.ISODateTime;
+import ca.gc.aafc.collection.api.dto.CollectingEventDto;
+import ca.gc.aafc.dina.dto.ExternalRelationDto;
+import ca.gc.aafc.dina.testsupport.BaseRestAssuredTest;
+import ca.gc.aafc.dina.testsupport.PostgresTestContainerInitializer;
+import ca.gc.aafc.dina.testsupport.jsonapi.JsonAPITestHelper;
+import ca.gc.aafc.dina.testsupport.specs.OpenAPI3Assertions;
+import lombok.SneakyThrows;
+import org.apache.http.client.utils.URIBuilder;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+
+import javax.transaction.Transactional;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@SpringBootTest(
+  classes = CollectionModuleApiLauncher.class,
+  webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@TestPropertySource(properties = "spring.config.additional-location=classpath:application-test.yml")
+@Transactional
+@ContextConfiguration(initializers = {PostgresTestContainerInitializer.class})
+public class CollectingEventOpenApiIT extends BaseRestAssuredTest {
+
+  private static final String SPEC_HOST = "raw.githubusercontent.com";
+  private static final String SPEC_PATH = "DINA-Web/collection-specs/master/schema/collectingEvent.yml";
+  private static final URIBuilder URI_BUILDER = new URIBuilder();
+
+  public static final String TYPE_NAME = "collecting-event";
+
+  static {
+    URI_BUILDER.setScheme("https");
+    URI_BUILDER.setHost(SPEC_HOST);
+    URI_BUILDER.setPath(SPEC_PATH);
+  }
+
+  protected CollectingEventOpenApiIT() {
+    super("/api/v1/");
+  }
+
+  public static URL getOpenAPISpecsURL() throws URISyntaxException, MalformedURLException {
+    return URI_BUILDER.build().toURL();
+  }
+
+  @SneakyThrows
+  @Test
+  void collectingEvent_SpecValid() {
+    CollectingEventDto ce = new CollectingEventDto();
+    ce.setGroup("test group");
+    ce.setDecimalLatitude(3.2);
+    ce.setDecimalLongitude(1.2);
+    ce.setVerbatimEventDateTime("a cold winter morning in the winter of 2099");
+    ce.setCoordinateUncertaintyInMeters(2);
+    ce.setCollectorGroupUuid(UUID.randomUUID());
+    ce.setVerbatimCollectors("Jack and Jane");
+    ce.setStartEventDateTime(ISODateTime.parse("2007-12-03T10:15:30").toString());
+    ce.setEndEventDateTime(ISODateTime.parse("2007-12-04T11:20:20").toString());
+    ce.setVerbatimCoordinates("26.089, 106.36");
+    ce.setCollectors(null);
+    ce.setAttachment(null);
+
+    OpenAPI3Assertions.assertRemoteSchema(getOpenAPISpecsURL(), "CollectingEvent",
+      sendPost(TYPE_NAME, JsonAPITestHelper.toJsonAPIMap(TYPE_NAME, JsonAPITestHelper.toAttributeMap(ce),
+        Map.of(
+          "collectors", getExternalListType("agent"),
+          "attachment", getExternalListType("metadata")),
+        null)
+      ).extract().asString());
+  }
+
+  private Map<String, Object> getExternalListType(String type) {
+    return Map.of("data", List.of(Map.of(
+      "id", UUID.randomUUID().toString(),
+      "type", type)));
+  }
+
+}
