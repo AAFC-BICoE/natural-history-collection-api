114475ab753fa7f72be7817991b65ea4b5b432c1
Jkeyuk <jonkero2@gmail.com>
2021-02-01 07:58:39 -0500

    21423 implement search event date time
    - slight refactor to unused data type
    - test clean up

--- src/main/java/ca/gc/aafc/collection/api/datetime/IsoRsqlPrecisionResolver.java
+++ src/main/java/ca/gc/aafc/collection/api/datetime/IsoRsqlPrecisionResolver.java
@@ -10,94 +10,61 @@ import cz.jirutka.rsql.parser.ast.OrNode;
 import cz.jirutka.rsql.parser.ast.RSQLOperators;
 import cz.jirutka.rsql.parser.ast.RSQLVisitor;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-/**
- * Rsql visitor to support rsql date filtering with partial dates. Partial dates are resolved using {@link
- * ISODateTime#parse(String)}
- */
 @AllArgsConstructor
-public class IsoRsqlPrecisionResolver implements RSQLVisitor<IsoRsqlPrecisionResolver.PrecisionNode, Set<String>> {
+public class IsoRsqlPrecisionResolver implements RSQLVisitor<Node, Set<String>> {
 
   private static final RSQLParser RSQL_PARSER = new RSQLParser();
   private final Map<String, String> fieldToPrecisionNameMap;
 
-  /**
-   * Returns the given rsql string with partial dates resolved for fields tracked by the resolver. Partial
-   * dates are resolved using {@link * ISODateTime#parse(String)}
-   *
-   * @param rsql rsql string to resolve
-   * @return the given rsql string with partial dates resolved
-   */
   public String resolveDates(String rsql) {
     return RSQL_PARSER.parse(rsql).accept(this, fieldToPrecisionNameMap.keySet()).toString();
   }
 
   @Override
-  public PrecisionNode visit(AndNode andNode, Set<String> field) {
+  public Node visit(AndNode andNode, Set<String> field) {
     return resolveLogicalNode(andNode, field);
   }
 
   @Override
-  public PrecisionNode visit(OrNode orNode, Set<String> field) {
+  public Node visit(OrNode orNode, Set<String> field) {
     return resolveLogicalNode(orNode, field);
   }
 
-  private PrecisionNode resolveLogicalNode(LogicalNode logicalNode, Set<String> field) {
-    List<Node> nodes = new ArrayList<>();
-    for (Node node : logicalNode.getChildren()) {
-      PrecisionNode accept = node.accept(this, field);
-      nodes.add(accept.getNode());
-    }
-    return PrecisionNode.builder()
-      .node(logicalNode.withChildren(nodes))
-      .build();
+  private Node resolveLogicalNode(LogicalNode logicalNode, Set<String> field) {
+    return logicalNode.withChildren(logicalNode.getChildren()
+      .stream()
+      .map(node -> node.accept(this, field))
+      .collect(Collectors.toList()));
   }
 
   @Override
-  public PrecisionNode visit(ComparisonNode node, Set<String> field) {
+  public Node visit(ComparisonNode node, Set<String> field) {
     if (field.stream().anyMatch(f -> node.getSelector().equalsIgnoreCase(f))) {
-      ISODateTime argument = node.getArguments().stream().findFirst().map(ISODateTime::parse).orElseThrow();
-      LocalDateTime lowerBound = argument.getLocalDateTime();
-      LocalDateTime upperBound = getUpperBound(lowerBound, argument.getFormat());
       if (node.getOperator().equals(RSQLOperators.EQUAL)) {
-        return PrecisionNode.builder()
-          .node(newBoundedNode(node, lowerBound, upperBound, node.getSelector()))
-          .build();
+        ISODateTime argument = node.getArguments().stream().findFirst().map(ISODateTime::parse).orElseThrow();
+        LocalDateTime lowerBound = argument.getLocalDateTime();
+        LocalDateTime upperBound = getUpperBound(lowerBound, argument.getFormat());
+        return newBoundedNode(lowerBound, upperBound, node.getSelector());
       } else {
-        return PrecisionNode.builder()
-          .node(new ComparisonNode(node.getOperator(),
-            node.getSelector(),
-            List.of(argument.getLocalDateTime().toString())))
-          .build();
+        return node;
       }
     } else {
-      return PrecisionNode.builder().node(node).build();
+      return node;
     }
   }
 
-  private AndNode newBoundedNode(
-    ComparisonNode node,
-    LocalDateTime lowerBound,
-    LocalDateTime upperBound,
-    String selector
-  ) {
+  private static AndNode newBoundedNode(LocalDateTime lower, LocalDateTime upper, String selector) {
     return new AndNode(List.of(
-      new ComparisonNode(
-        RSQLOperators.GREATER_THAN_OR_EQUAL,
-        selector,
-        List.of(lowerBound.toString())),
-      new ComparisonNode(
-        RSQLOperators.LESS_THAN_OR_EQUAL,
-        node.getSelector(),
-        List.of(upperBound.toString()))));
+      new ComparisonNode(RSQLOperators.GREATER_THAN_OR_EQUAL, selector, List.of(lower.toString())),
+      new ComparisonNode(RSQLOperators.LESS_THAN_OR_EQUAL, selector, List.of(upper.toString())))
+    );
   }
 
   private static LocalDateTime getUpperBound(LocalDateTime lowerBound, Format format) {
@@ -125,15 +92,4 @@ public class IsoRsqlPrecisionResolver implements RSQLVisitor<IsoRsqlPrecisionRes
     return upperBound;
   }
 
-  @Builder
-  @Getter
-  public static final class PrecisionNode {
-    private final Node node;
-
-    @Override
-    public String toString() {
-      System.out.println(this.getNode().toString());
-      return this.getNode().toString();
-    }
-  }
 }
--- src/test/java/ca/gc/aafc/collection/api/datetime/IsoRsqlPrecisionResolverTest.java
+++ src/test/java/ca/gc/aafc/collection/api/datetime/IsoRsqlPrecisionResolverTest.java
@@ -1,7 +1,6 @@
 package ca.gc.aafc.collection.api.datetime;
 
 import cz.jirutka.rsql.parser.RSQLParser;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
@@ -9,15 +8,14 @@ import java.util.Map;
 class IsoRsqlPrecisionResolverTest {
 
   public static final RSQLParser RSQL_PARSER = new RSQLParser();
-  public static final Map<String, String> FIELD_LIST = Map.of("startEventDateTime",
+  public static final Map<String, String> FIELD_LIST = Map.of(
+    "startEventDateTime",
     "startEventDateTimePrecision");
 
   @Test
   void highestPercision() {
     IsoRsqlPrecisionResolver resolver = new IsoRsqlPrecisionResolver(FIELD_LIST);
-    IsoRsqlPrecisionResolver.PrecisionNode node = RSQL_PARSER.parse(
-      "startEventDateTime==1800-01")
-      .accept(resolver, FIELD_LIST.keySet());
-    System.out.println(node);
+    System.out.println(RSQL_PARSER.parse("startEventDateTime==1800-01")
+      .accept(resolver, FIELD_LIST.keySet()));
   }
 }
