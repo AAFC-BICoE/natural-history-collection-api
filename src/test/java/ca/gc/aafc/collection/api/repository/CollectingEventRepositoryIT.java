54f9bfd30cf041d0489f9c84fc012d7e18c557f0
Jkeyuk <jonkero2@gmail.com>
2021-02-01 09:40:02 -0500

    21423 implement search event date time
    - fix case

--- src/test/java/ca/gc/aafc/collection/api/repository/CollectingEventRepositoryIT.java
+++ src/test/java/ca/gc/aafc/collection/api/repository/CollectingEventRepositoryIT.java
@@ -134,7 +134,7 @@ public class CollectingEventRepositoryIT extends CollectionModuleBaseIT {
       Arguments.of("2021-03-03T03:00", "startEventDateTime==2000-03-03", 1),
       Arguments.of("2021-03-03T03:00", "startEventDateTime==2000-03-03T03:00", 1),
       Arguments.of("2021-03-03T03:00", "startEventDateTime==2000-03-03T02:59", 0),
-      Arguments.of("2021-03-03T03:00", "startEventDateTime==2000-03-03T04:00", 0)
+      Arguments.of("2021-03-03T03:00", "startEventDateTime==2000-03-03T03:01", 0)
     );
   }
 
