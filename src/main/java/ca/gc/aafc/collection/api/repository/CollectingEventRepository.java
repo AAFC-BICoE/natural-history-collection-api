416d73bc5b63672b626813bfc6200c61d077b677
xuemeigan <xuemei.gan@canada.ca>
2021-02-02 20:42:12 -0500

    Support 21571 Add GroupAuthorizationService on collecting-event - Fix Open Api test

--- src/main/java/ca/gc/aafc/collection/api/repository/CollectingEventRepository.java
+++ src/main/java/ca/gc/aafc/collection/api/repository/CollectingEventRepository.java
@@ -10,14 +10,8 @@ import ca.gc.aafc.dina.repository.DinaRepository;
 import ca.gc.aafc.dina.repository.external.ExternalResourceProvider;
 import ca.gc.aafc.dina.security.DinaAuthenticatedUser;
 import ca.gc.aafc.dina.service.DinaAuthorizationService;
-import ca.gc.aafc.dina.service.DinaService;
-import ca.gc.aafc.dina.service.GroupAuthorizationService;
 import lombok.NonNull;
-
 import java.util.Optional;
-
-import javax.inject.Inject;
-
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.stereotype.Repository;
 
