ac77100630dcafead47a8500d71ea8b1b418be4f
Jkeyuk <jonkero2@gmail.com>
2021-02-16 10:50:41 -0500

    21755 managed attribute
    add entity and crud it

---
+++ src/test/java/ca/gc/aafc/collection/api/entities/ManagedAttributeCRUDIT.java
@@ -0,0 +1,43 @@
+package ca.gc.aafc.collection.api.entities;
+
+import ca.gc.aafc.collection.api.CollectionModuleBaseIT;
+import ca.gc.aafc.dina.jpa.BaseDAO;
+import ca.gc.aafc.dina.service.DefaultDinaService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+import java.util.UUID;
+
+class ManagedAttributeCRUDIT extends CollectionModuleBaseIT {
+  @Inject
+  private BaseDAO baseDAO;
+  private DefaultDinaService<ManagedAttribute> service;
+
+  @BeforeEach
+  void setUp() {
+    service = new DefaultDinaService<>(baseDAO);
+  }
+
+  @Test
+  void create_recordCreated() {
+    String expectedValue = "value";
+    String expectedCreatedBy = "dina";
+    String expectedName = "dina test attribute";
+    UUID uuid = service.create(ManagedAttribute.builder()
+      .uuid(UUID.randomUUID())
+      .managedAttributeType(ManagedAttribute.ManagedAttributeType.STRING)
+      .acceptedValues(new String[]{expectedValue})
+      .createdBy(expectedCreatedBy)
+      .name(expectedName)
+      .build()).getUuid();
+    ManagedAttribute result = service.findOne(uuid, ManagedAttribute.class);
+    Assertions.assertNotNull(result.getId());
+    Assertions.assertNotNull(result.getCreatedOn());
+    Assertions.assertEquals(expectedCreatedBy, result.getCreatedBy());
+    Assertions.assertEquals(expectedName, result.getName());
+    Assertions.assertEquals(expectedValue, result.getAcceptedValues()[0]);
+  }
+
+}
