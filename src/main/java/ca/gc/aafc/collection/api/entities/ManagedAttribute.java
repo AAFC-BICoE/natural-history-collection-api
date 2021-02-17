0709897ade61a1b6d77b38dfb63f7b1c922c1c08
Jonathan Keyuk <Jonkero2@gmail.com>
2021-02-17 07:41:27 -0500

    21755 managed attribute entity (#37)
    * add migration
    * add entity and crud it
    * update vlad types to 52

---
+++ src/main/java/ca/gc/aafc/collection/api/entities/ManagedAttribute.java
@@ -0,0 +1,89 @@
+package ca.gc.aafc.collection.api.entities;
+
+import ca.gc.aafc.dina.entity.DinaEntity;
+import com.vladmihalcea.hibernate.type.array.StringArrayType;
+import com.vladmihalcea.hibernate.type.basic.PostgreSQLEnumType;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Generated;
+import org.hibernate.annotations.GenerationTime;
+import org.hibernate.annotations.NaturalId;
+import org.hibernate.annotations.NaturalIdCache;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@TypeDefs({
+  @TypeDef(name = "pgsql_enum", typeClass = PostgreSQLEnumType.class),
+  @TypeDef(name = "string-array", typeClass = StringArrayType.class)})
+@AllArgsConstructor
+@Builder
+@RequiredArgsConstructor
+@SuppressFBWarnings(justification = "ok for Hibernate Entity", value = {"EI_EXPOSE_REP", "EI_EXPOSE_REP2"})
+@NaturalIdCache
+public class ManagedAttribute implements DinaEntity {
+
+  public enum ManagedAttributeType {
+    INTEGER, STRING
+  }
+
+  public enum ManagedAttributeComponent {
+    COLLECTING_EVENT
+  }
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @NaturalId
+  @NotNull
+  @Column(name = "uuid", unique = true)
+  private UUID uuid;
+
+  @NotBlank
+  private String name;
+
+  @NotNull
+  @Type(type = "pgsql_enum")
+  @Enumerated(EnumType.STRING)
+  @Column(name = "type")
+  private ManagedAttributeType managedAttributeType;
+
+  @NotNull
+  @Type(type = "pgsql_enum")
+  @Enumerated(EnumType.STRING)
+  @Column(name = "component")
+  private ManagedAttributeComponent managedAttributeComponent;
+
+  @Type(type = "string-array")
+  @Column(columnDefinition = "text[]")
+  private String[] acceptedValues;
+
+  @Column(name = "created_on", insertable = false, updatable = false)
+  @Generated(value = GenerationTime.INSERT)
+  private OffsetDateTime createdOn;
+
+  @NotBlank
+  @Column(name = "created_by", updatable = false)
+  private String createdBy;
+
+}
