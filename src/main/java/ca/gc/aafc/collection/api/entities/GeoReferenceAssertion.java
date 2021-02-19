37d16aa982d74e2755e00328885b63bd8c83b6b3
Shemy Gan <44707236+xuemeigan@users.noreply.github.com>
2021-02-19 08:06:28 -0500

    Support 21764 add georeference assertion entity (#39)
    * Support 21764 Add georeference assertion entity
    - Add migration log, entity and dto
    
    * Support 21764 Add georeference assertion entity
    - Remove unused imports
    
    * Support 21764 Add georeference assertion entity
    - Add test

---
+++ src/main/java/ca/gc/aafc/collection/api/entities/GeoReferenceAssertion.java
@@ -0,0 +1,60 @@
+package ca.gc.aafc.collection.api.entities;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.Generated;
+import org.hibernate.annotations.GenerationTime;
+import org.hibernate.annotations.NaturalId;
+import org.hibernate.annotations.NaturalIdCache;
+
+import ca.gc.aafc.dina.entity.DinaEntity;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@Builder
+@Setter
+@Getter
+@RequiredArgsConstructor
+@SuppressFBWarnings(justification = "ok for Hibernate Entity", value = { "EI_EXPOSE_REP", "EI_EXPOSE_REP2" })
+@NaturalIdCache
+@Table(name = "georeference_assertion")
+public class GeoReferenceAssertion implements DinaEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @NaturalId
+  @NotNull
+  @Column(unique = true)
+  private UUID uuid;
+
+  private Double dwcDecimalLatitude;
+  private Double dwcDecimalLongitude;
+  private Integer dwcCoordinateUncertaintyInMeters;
+
+  @Column(name = "created_on", insertable = false, updatable = false)
+  @Generated(value = GenerationTime.INSERT)
+  private OffsetDateTime createdOn;
+
+  @NotBlank
+  @Column(name = "created_by", updatable = false)
+  private String createdBy;  
+
+}
