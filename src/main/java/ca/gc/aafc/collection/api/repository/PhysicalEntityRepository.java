9a390cc221f88e38a781e0a6ea0c9cce94977ed9
luusteve <luu.stevenluu@gmail.com>
2021-03-29 11:22:30 -0400

    Added initial physical-entity

---
+++ src/main/java/ca/gc/aafc/collection/api/repository/PhysicalEntityRepository.java
@@ -0,0 +1,45 @@
+package ca.gc.aafc.collection.api.repository;
+
+import java.util.Optional;
+
+import org.springframework.boot.info.BuildProperties;
+
+import ca.gc.aafc.collection.api.dto.PhysicalEntityDto;
+import ca.gc.aafc.collection.api.entities.PhysicalEntity;
+import ca.gc.aafc.collection.api.service.PhysicalEntityService;
+import ca.gc.aafc.dina.mapper.DinaMapper;
+import ca.gc.aafc.dina.repository.DinaRepository;
+import ca.gc.aafc.dina.repository.external.ExternalResourceProvider;
+import ca.gc.aafc.dina.security.DinaAuthenticatedUser;
+import lombok.NonNull;
+
+public class PhysicalEntityRepository extends DinaRepository<PhysicalEntityDto, PhysicalEntity> {
+    
+    private Optional<DinaAuthenticatedUser> dinaAuthenticatedUser;  
+
+    public PhysicalEntityRepository(
+        @NonNull PhysicalEntityService dinaService,
+        ExternalResourceProvider externalResourceProvider,
+        @NonNull BuildProperties buildProperties,
+        Optional<DinaAuthenticatedUser> dinaAuthenticatedUser
+    ) {
+        super(
+            dinaService,
+            Optional.empty(),
+            Optional.empty(),
+            new DinaMapper<>(PhysicalEntityDto.class),
+            PhysicalEntityDto.class,
+            PhysicalEntity.class,
+            null,
+            externalResourceProvider,
+            buildProperties);
+        this.dinaAuthenticatedUser = dinaAuthenticatedUser;
+    }
+
+    @Override
+    public <S extends PhysicalEntityDto> S create(S resource) {
+      dinaAuthenticatedUser.ifPresent(
+        authenticatedUser -> resource.setCreatedBy(authenticatedUser.getUsername()));
+      return super.create(resource);
+    }
+}
