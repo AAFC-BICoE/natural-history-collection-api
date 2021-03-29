05f57c990623152043a5035fc8f2cfb883e0d8d3
Jkeyuk <jonkero2@gmail.com>
2021-03-29 11:18:53 -0400

    21388 add initial Preparation Process
    create PreparationProcessService.java

--- src/test/java/ca/gc/aafc/collection/api/entities/PreparationProcessCRUDIT.java
+++ src/test/java/ca/gc/aafc/collection/api/entities/PreparationProcessCRUDIT.java
@@ -1,6 +1,7 @@
 package ca.gc.aafc.collection.api.entities;
 
 import ca.gc.aafc.collection.api.CollectionModuleBaseIT;
+import ca.gc.aafc.collection.api.service.PreparationProcessService;
 import ca.gc.aafc.dina.jpa.BaseDAO;
 import ca.gc.aafc.dina.service.DefaultDinaService;
 import org.junit.jupiter.api.Assertions;
@@ -8,7 +9,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
-import java.util.UUID;
 
 public class PreparationProcessCRUDIT extends CollectionModuleBaseIT {
   @Inject
@@ -17,17 +17,17 @@ public class PreparationProcessCRUDIT extends CollectionModuleBaseIT {
 
   @BeforeEach
   void setUp() {
-    maService = new DefaultDinaService<>(baseDAO);
+    maService = new PreparationProcessService(baseDAO);
   }
 
   @Test
   void create() {
     PreparationProcess build = PreparationProcess.builder()
       .createdBy("dina")
-      .uuid(UUID.randomUUID())
       .build();
     maService.create(build);
     Assertions.assertNotNull(build.getId());
     Assertions.assertNotNull(build.getCreatedOn());
+    Assertions.assertNotNull(build.getUuid());
   }
 }
