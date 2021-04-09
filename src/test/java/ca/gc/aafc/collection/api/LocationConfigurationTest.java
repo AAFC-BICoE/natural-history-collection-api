e20486652d0af41bf27b1d1bcea86f3254bdd13b
Jkeyuk <jonkero2@gmail.com>
2021-04-09 10:33:16 -0400

    21388 add initial Preparation Process
    LocationConfigurationTest.java

---
+++ src/test/java/ca/gc/aafc/collection/api/LocationConfigurationTest.java
@@ -0,0 +1,32 @@
+package ca.gc.aafc.collection.api;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.inject.Inject;
+
+@SpringBootTest(
+  classes = CollectionModuleApiLauncher.class,
+  webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+class LocationConfigurationTest extends CollectionModuleBaseIT {
+  @Inject
+  private LocationConfiguration locationConfiguration;
+
+  @Test
+  void getCoordinateSystem() {
+    MatcherAssert.assertThat(
+      locationConfiguration.getCoordinateSystem(),
+      Matchers.contains(
+        "decimal degrees",
+        "degrees decimal minutes",
+        "degrees minutes seconds",
+        "UTM"));
+  }
+
+  @Test
+  void getSrs() {
+  }
+}
