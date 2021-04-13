a3246ece63f8ae20a20a94139bc5a08c6a8f86d2
luusteve <luu.stevenluu@gmail.com>
2021-04-13 16:51:52 -0400

    Revert return after illegalArgumentException

--- src/main/java/ca/gc/aafc/collection/api/validation/CollectingEventValidator.java
+++ src/main/java/ca/gc/aafc/collection/api/validation/CollectingEventValidator.java
@@ -36,19 +36,16 @@ public class CollectingEventValidator implements Validator {
           String errorMessage = messageSource.getMessage("validation.constraint.violation.validEventDateTime", null,
             LocaleContextHolder.getLocale());
           errors.reject("validation.constraint.violation.validEventDateTime", errorMessage);
-          return;
         }
       if (assertionsSize > 0 && collectingEvent.getPrimaryGeoreferenceAssertion() == null) {
           String errorMessage = messageSource.getMessage("collectingEvent.primaryGeoreferenceAssertion.null",
           null, LocaleContextHolder.getLocale());
           errors.rejectValue("primaryGeoreferenceAssertion", "collectingEvent.primaryGeoreferenceAssertion.null", errorMessage);
-          return;
       } 
-      if (collectingEvent.getGeoReferenceAssertions().contains(collectingEvent.getPrimaryGeoreferenceAssertion())) {
+      if (assertionsSize > 0 && collectingEvent.getGeoReferenceAssertions().contains(collectingEvent.getPrimaryGeoreferenceAssertion())) {
           String errorMessage = messageSource.getMessage("collectingEvent.primaryGeoreferenceAssertion.inList",
           null, LocaleContextHolder.getLocale());
           errors.rejectValue("primaryGeoreferenceAssertion", "collectingEvent.primaryGeoreferenceAssertion.inList", errorMessage);
-          return;
       }
     }
 
