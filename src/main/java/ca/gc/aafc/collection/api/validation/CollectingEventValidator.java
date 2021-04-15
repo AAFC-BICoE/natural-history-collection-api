b68afc4246786379e7744b2a1f0b5770cfd5b003
Jkeyuk <jonkero2@gmail.com>
2021-04-15 13:38:00 -0400

    22345 revert primary assertion
    adjust classes for missing field

--- src/main/java/ca/gc/aafc/collection/api/validation/CollectingEventValidator.java
+++ src/main/java/ca/gc/aafc/collection/api/validation/CollectingEventValidator.java
@@ -1,53 +1,41 @@
 package ca.gc.aafc.collection.api.validation;
 
-import java.util.List;
-import java.util.Optional;
-
+import ca.gc.aafc.collection.api.entities.CollectingEvent;
+import lombok.NonNull;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
-import ca.gc.aafc.collection.api.entities.CollectingEvent;
-
 @Component
 public class CollectingEventValidator implements Validator {
-    
-    private MessageSource messageSource;
 
-    public CollectingEventValidator(MessageSource messageSource) {
-      this.messageSource = messageSource;
+  private final MessageSource messageSource;
+
+  public CollectingEventValidator(MessageSource messageSource) {
+    this.messageSource = messageSource;
+  }
+
+  @Override
+  public boolean supports(@NonNull Class<?> clazz) {
+    return CollectingEvent.class.isAssignableFrom(clazz);
+  }
+
+  @Override
+  public void validate(@NonNull Object target, @NonNull Errors errors) {
+    if (!supports(target.getClass())) {
+      throw new IllegalArgumentException("CollectingEventValidator not supported for class " + target.getClass());
     }
-
-    @Override
-    public boolean supports(Class<?> clazz) {
-      return CollectingEvent.class.isAssignableFrom(clazz);
+    CollectingEvent collectingEvent = (CollectingEvent) target;
+    if ((collectingEvent.getStartEventDateTime() == null && collectingEvent.getEndEventDateTime() != null)
+      || (collectingEvent.getEndEventDateTime() != null
+      && collectingEvent.getStartEventDateTime().isAfter(collectingEvent.getEndEventDateTime()))) {
+      String errorMessage = messageSource.getMessage(
+        "validation.constraint.violation.validEventDateTime",
+        null,
+        LocaleContextHolder.getLocale());
+      errors.reject("validation.constraint.violation.validEventDateTime", errorMessage);
     }
-
-    @Override
-    public void validate(Object target, Errors errors) {
-      CollectingEvent collectingEvent = (CollectingEvent) target;
-      int assertionsSize = Optional.ofNullable(collectingEvent.getGeoReferenceAssertions())
-      .map(List::size).orElse(0);
-      if ((collectingEvent.getStartEventDateTime() == null && collectingEvent.getEndEventDateTime() != null)
-            || (collectingEvent.getEndEventDateTime() != null
-              && collectingEvent.getStartEventDateTime().isAfter(collectingEvent.getEndEventDateTime()))) {
-          String errorMessage = messageSource.getMessage("validation.constraint.violation.validEventDateTime", null,
-            LocaleContextHolder.getLocale());
-          errors.reject("validation.constraint.violation.validEventDateTime", errorMessage);
-        }
-      if (assertionsSize > 0 && collectingEvent.getPrimaryGeoreferenceAssertion() == null) {
-          String errorMessage = messageSource.getMessage("collectingEvent.primaryGeoreferenceAssertion.null",
-          null, LocaleContextHolder.getLocale());
-          errors.rejectValue("primaryGeoreferenceAssertion", "collectingEvent.primaryGeoreferenceAssertion.null", errorMessage);
-      } 
-      if (assertionsSize > 0 && collectingEvent.getGeoReferenceAssertions().contains(collectingEvent.getPrimaryGeoreferenceAssertion())) {
-          String errorMessage = messageSource.getMessage("collectingEvent.primaryGeoreferenceAssertion.inList",
-          null, LocaleContextHolder.getLocale());
-          errors.rejectValue("primaryGeoreferenceAssertion", "collectingEvent.primaryGeoreferenceAssertion.inList", errorMessage);
-      }
-    }
-
-    
+  }
 }
