29979db48a28d4707b2be787c12d2063d8a226cb
PoffM <matp@live.ca>
2021-04-16 16:39:47 -0400

    Support 22095 move managed attributes to jsonb based column (#79)
    -Removed CollectingEventManagedAttribute entity and dto and table.
    -Added managedAttributeValues field+column+migration.
    -Added componentType+key findOne lookup to ManagedAttributeRepo.
    -Fixed Map type in ManagedAttributeDTO: Map<String, ManagedAttributeValue>
    -Updated StringMapper in ManagedAttributeDto config to accept the new identifier format.
    -Added 404 exception when Managed Attribute is not found using the new identifier format.
    -Removed @Null(groups = OnCreate.class) annotation to fix API requests through operations module Other entitites do not have this either.

--- src/main/java/ca/gc/aafc/collection/api/entities/ManagedAttribute.java
+++ src/main/java/ca/gc/aafc/collection/api/entities/ManagedAttribute.java
@@ -1,23 +1,7 @@
 package ca.gc.aafc.collection.api.entities;
 
-import ca.gc.aafc.dina.entity.DinaEntity;
-import ca.gc.aafc.dina.service.OnCreate;
-import ca.gc.aafc.dina.service.OnUpdate;
-import com.vladmihalcea.hibernate.type.array.StringArrayType;
-import com.vladmihalcea.hibernate.type.basic.PostgreSQLEnumType;
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import org.hibernate.annotations.Generated;
-import org.hibernate.annotations.GenerationTime;
-import org.hibernate.annotations.NaturalId;
-import org.hibernate.annotations.NaturalIdCache;
-import org.hibernate.annotations.Type;
-import org.hibernate.annotations.TypeDef;
-import org.hibernate.annotations.TypeDefs;
+import java.time.OffsetDateTime;
+import java.util.UUID;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -26,14 +10,31 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 import javax.validation.constraints.Size;
-import java.time.OffsetDateTime;
-import java.util.List;
-import java.util.UUID;
+
+import com.vladmihalcea.hibernate.type.array.StringArrayType;
+import com.vladmihalcea.hibernate.type.basic.PostgreSQLEnumType;
+
+import org.hibernate.annotations.Generated;
+import org.hibernate.annotations.GenerationTime;
+import org.hibernate.annotations.NaturalId;
+import org.hibernate.annotations.NaturalIdCache;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
+
+import ca.gc.aafc.dina.entity.DinaEntity;
+import ca.gc.aafc.dina.service.OnCreate;
+import ca.gc.aafc.dina.service.OnUpdate;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
@@ -61,7 +62,6 @@ public class ManagedAttribute implements DinaEntity {
   private Integer id;
 
   @NaturalId
-  @Null(groups = OnCreate.class)
   @NotNull(groups = OnUpdate.class)
   @Column(name = "uuid", unique = true)
   private UUID uuid;
@@ -100,8 +100,5 @@ public class ManagedAttribute implements DinaEntity {
   @NotBlank
   @Column(name = "created_by", updatable = false)
   private String createdBy;
-
-  @OneToMany(mappedBy = "event")
-  private List<CollectingEventManagedAttribute> managedAttributes;
 
 }
