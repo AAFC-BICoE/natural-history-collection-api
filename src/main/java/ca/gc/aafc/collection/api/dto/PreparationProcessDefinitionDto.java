27ddf10fbeae2ca8e04e5d83ac464677427a92ee
luusteve <45112541+luusteve@users.noreply.github.com>
2021-04-26 10:32:42 -0400

    Created Preparation Process Resources (#86)

---
+++ src/main/java/ca/gc/aafc/collection/api/dto/PreparationProcessDefinitionDto.java
@@ -0,0 +1,38 @@
+package ca.gc.aafc.collection.api.dto;
+
+import ca.gc.aafc.collection.api.entities.PreparationProcessDefinition;
+import ca.gc.aafc.dina.dto.RelatedEntity;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+import io.crnk.core.resource.annotations.JsonApiId;
+import io.crnk.core.resource.annotations.JsonApiResource;
+
+import java.time.OffsetTime;
+import java.util.UUID;
+
+import org.javers.core.metamodel.annotation.Id;
+import org.javers.core.metamodel.annotation.PropertyName;
+
+import lombok.Data;
+
+@Data
+@RelatedEntity(PreparationProcessDefinition.class)
+@SuppressFBWarnings({"EI_EXPOSE_REP", "EI_EXPOSE_REP2"})
+@JsonApiResource(type = PreparationProcessDefinitionDto.TYPENAME)
+public class PreparationProcessDefinitionDto {
+
+  public static final String TYPENAME = "preparation-process-definition";
+  
+  @JsonApiId
+  @Id
+  @PropertyName("id")
+  private UUID uuid;
+
+  private OffsetTime createdOn;
+
+  private String createdBy;
+
+  private String name;
+
+  private String group;
+}
---
+++ src/main/java/ca/gc/aafc/collection/api/dto/PreparationProcessDto.java
@@ -0,0 +1,52 @@
+package ca.gc.aafc.collection.api.dto;
+
+import ca.gc.aafc.dina.mapper.IgnoreDinaMapping;
+import ca.gc.aafc.collection.api.entities.PreparationProcess;
+import ca.gc.aafc.dina.dto.RelatedEntity;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+import io.crnk.core.resource.annotations.JsonApiId;
+import io.crnk.core.resource.annotations.JsonApiRelation;
+import io.crnk.core.resource.annotations.JsonApiResource;
+
+import java.time.OffsetTime;
+import java.util.UUID;
+
+import org.javers.core.metamodel.annotation.Id;
+import org.javers.core.metamodel.annotation.PropertyName;
+
+import lombok.Data;
+
+@Data
+@RelatedEntity(PreparationProcess.class)
+@SuppressFBWarnings({"EI_EXPOSE_REP", "EI_EXPOSE_REP2"})
+@JsonApiResource(type = PreparationProcessDto.TYPENAME)
+public class PreparationProcessDto {
+
+  public static final String TYPENAME = "preparation-process";
+
+  @JsonApiId
+  @Id
+  @PropertyName("id")
+  private UUID uuid;
+
+  private OffsetTime createdOn;
+
+  private String createdBy;
+
+  private UUID agentId;
+
+  @IgnoreDinaMapping
+  private String startDateTime;
+
+  @IgnoreDinaMapping
+  private String endDateTime;
+
+  @JsonApiRelation
+  private PreparationProcessDefinitionDto preparationProcessDefinition;
+
+  @JsonApiRelation
+  private PhysicalEntityDto sourcePhysicalEntity;
+  
+}
