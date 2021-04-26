ede2d19f855b911ea7e23be8c734e985b61a5f11
luusteve <luu.stevenluu@gmail.com>
2021-04-26 13:42:38 -0400

    added initial preparationProcessRepositoryIT

---
+++ src/test/java/ca/gc/aafc/collection/api/repository/PreparationProcessRepositoryIT.java
@@ -0,0 +1,44 @@
+package ca.gc.aafc.collection.api.repository;
+
+import ca.gc.aafc.collection.api.CollectionModuleBaseIT;
+import ca.gc.aafc.collection.api.datetime.ISODateTime;
+import ca.gc.aafc.collection.api.dto.CollectingEventDto;
+import ca.gc.aafc.collection.api.dto.PhysicalEntityDto;
+import ca.gc.aafc.dina.dto.ExternalRelationDto;
+import java.util.List;
+import java.util.UUID;
+
+import javax.inject.Inject;
+
+@SpringBootTest(properties = "keycloak.enabled=true")
+public class PreparationProcessRepositoryIT extends CollectionModuleBaseIT {
+
+  // @Inject 
+  // private PreparationProcessRepository preparationProcessRepository
+
+  // For Physical Entity
+  private static final String dwcCatalogNumber = "R-4313";
+  private static final String group = "aafc";
+
+  
+  private PhysicalEntityDto newPhysicalEntity(
+    String dwcCatalogNumber, 
+    CollectingEventDto event) {
+    PhysicalEntityDto pe = new PhysicalEntityDto();
+    pe.setDwcCatalogNumber(dwcCatalogNumber);
+    pe.setCollectingEvent(event);
+    pe.setGroup(group);
+    pe.setAttachment(List.of(
+        ExternalRelationDto.builder().id(UUID.randomUUID().toString()).type("metadata").build()));
+    return pe;
+}
+
+  private CollectingEventDto newEventDto() {
+    CollectingEventDto ce = new CollectingEventDto();
+    ce.setGroup(group);
+    ce.setStartEventDateTime(ISODateTime.parse("2020").toString());
+    ce.setCreatedBy("dina");
+    return ce;
+  }
+  
+}
