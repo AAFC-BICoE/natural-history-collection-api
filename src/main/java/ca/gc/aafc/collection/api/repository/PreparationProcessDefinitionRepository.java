0635b3323faba98268ad4e38cf2008a2ab89bc97
luusteve <45112541+luusteve@users.noreply.github.com>
2021-04-28 11:42:01 -0400

    22477 create repositories for prep process classes (#88)
    * added initial preparationProcessRepositoryIT
    
    * Add initial preparationProcess repos
    
    * repository annotation in preprationProcessDefinition
    
    * Created testsupport factories for all 3 preparation process classes
    
    * Preparation Process Repository IT created
    
    * Add creation test PreparationProcessRepositoryIT
    
    * IT for prep process repositories
    
    * ReAdd accidentally removed @NotNull annotation for field UUID

---
+++ src/main/java/ca/gc/aafc/collection/api/repository/PreparationProcessDefinitionRepository.java
@@ -0,0 +1,48 @@
+package ca.gc.aafc.collection.api.repository;
+
+import java.util.Optional;
+
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.stereotype.Repository;
+
+import ca.gc.aafc.collection.api.dto.PreparationProcessDefinitionDto;
+import ca.gc.aafc.collection.api.entities.PreparationProcessDefinition;
+import ca.gc.aafc.collection.api.service.PreparationProcessDefinitionService;
+import ca.gc.aafc.dina.mapper.DinaMapper;
+import ca.gc.aafc.dina.repository.DinaRepository;
+import ca.gc.aafc.dina.repository.external.ExternalResourceProvider;
+import ca.gc.aafc.dina.security.DinaAuthenticatedUser;
+
+import lombok.NonNull;
+
+@Repository
+public class PreparationProcessDefinitionRepository extends DinaRepository<PreparationProcessDefinitionDto, PreparationProcessDefinition> {
+  
+  private Optional<DinaAuthenticatedUser> dinaAuthenticatedUser;  
+
+  public PreparationProcessDefinitionRepository(
+    @NonNull PreparationProcessDefinitionService dinaService,
+    ExternalResourceProvider externalResourceProvider,
+    @NonNull BuildProperties buildProperties,
+    Optional<DinaAuthenticatedUser> dinaAuthenticatedUser
+  ) {
+      super(
+          dinaService,
+          Optional.empty(),
+          Optional.empty(),
+          new DinaMapper<>(PreparationProcessDefinitionDto.class),
+          PreparationProcessDefinitionDto.class,
+          PreparationProcessDefinition.class,
+          null,
+          externalResourceProvider,
+          buildProperties);
+      this.dinaAuthenticatedUser = dinaAuthenticatedUser;
+  }
+  @Override
+  public <S extends PreparationProcessDefinitionDto> S create(S resource) {
+    dinaAuthenticatedUser.ifPresent(
+      authenticatedUser -> resource.setCreatedBy(authenticatedUser.getUsername()));
+    return super.create(resource);
+  }
+  
+}
