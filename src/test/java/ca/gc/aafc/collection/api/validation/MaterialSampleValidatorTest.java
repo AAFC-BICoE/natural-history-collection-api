4b12be5940d53689d0c3ea2d1b235dc39cd61c83
Jkeyuk <jonkero2@gmail.com>
2021-05-25 08:35:57 -0400

    22760 material sample validation
    add test suite

---
+++ src/test/java/ca/gc/aafc/collection/api/validation/MaterialSampleValidatorTest.java
@@ -0,0 +1,31 @@
+package ca.gc.aafc.collection.api.validation;
+
+import ca.gc.aafc.collection.api.CollectionModuleBaseIT;
+import ca.gc.aafc.collection.api.entities.MaterialSample;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
+import org.testcontainers.shaded.org.apache.commons.lang.RandomStringUtils;
+
+import javax.inject.Inject;
+import java.util.UUID;
+
+class MaterialSampleValidatorTest extends CollectionModuleBaseIT {
+
+  @Inject
+  private MaterialSampleValidator sampleValidator;
+
+  @Test
+  void validate_WhenValid_NoErrors() {
+    MaterialSample sample = MaterialSample.builder()
+      .uuid(UUID.randomUUID())
+      .group(RandomStringUtils.randomAlphabetic(3))
+      .createdBy(RandomStringUtils.randomAlphabetic(3))
+      .build();
+    Errors errors = new BeanPropertyBindingResult(sample, "name");
+    sampleValidator.validate(sample, errors);
+    Assertions.assertFalse(errors.hasErrors());
+  }
+
+}
