a162147d98d42e8dda28b992fa66a72e3c4d1a6c
Jonathan Keyuk <Jonkero2@gmail.com>
2021-05-27 13:08:38 -0400

    22811 update open api specs collection (#114)
    * CollectionOpenApiIT test
    
    * 22811
    
    swap spec url
    
    * 22811
    
    swap spec url
    
    Co-authored-by: luusteve <luu.stevenluu@gmail.com>

--- src/test/java/ca/gc/aafc/collection/api/openapi/MaterialSampleActionDefinitionOpenApiIT.java
+++ src/test/java/ca/gc/aafc/collection/api/openapi/MaterialSampleActionDefinitionOpenApiIT.java
@@ -29,7 +29,7 @@ import java.net.URL;
 public class MaterialSampleActionDefinitionOpenApiIT extends BaseRestAssuredTest {
 
   private static final String SPEC_HOST = "raw.githubusercontent.com";
-  private static final String SPEC_PATH = "DINA-Web/collection-specs/master/schema/materialSampleActionDefinition.yml";
+  private static final String SPEC_PATH = "DINA-Web/collection-specs/master/schema/natural-history-collection-api.yml";
   private static final URIBuilder URI_BUILDER = new URIBuilder();
 
   public static final String TYPE_NAME = "material-sample-action-definition";
