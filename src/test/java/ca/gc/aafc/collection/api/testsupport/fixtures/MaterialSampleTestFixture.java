0c89f7d8764d1b11aedeaa324064321ab583c57d
luusteve <45112541+luusteve@users.noreply.github.com>
2021-06-10 09:38:22 -0400

    Created MaterialSampleTestFixture (#132)

---
+++ src/test/java/ca/gc/aafc/collection/api/testsupport/fixtures/MaterialSampleTestFixture.java
@@ -0,0 +1,34 @@
+package ca.gc.aafc.collection.api.testsupport.fixtures;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+import ca.gc.aafc.collection.api.dto.MaterialSampleDto;
+import ca.gc.aafc.dina.dto.ExternalRelationDto;
+
+public class MaterialSampleTestFixture {
+
+  public static final String DWC_CATALOG_NUMBER = "R-4313";
+  public static final String[] DWC_OTHER_CATALOG_NUMBERS= new String[]{"A-1111", "B-2222"};
+  public static final String GROUP = "aafc";
+  public static final String MATERIAL_SAMPLE_NAME = "ocean water sample";
+  public static final UUID PREPARED_BY = UUID.randomUUID();
+  public static final LocalDate PREPARATION_DATE = LocalDate.now();
+  public static final String CREATED_BY = "test user";
+
+  public static MaterialSampleDto newMaterialSample() {
+    MaterialSampleDto materialSampleDto= new MaterialSampleDto();
+    materialSampleDto.setDwcCatalogNumber(DWC_CATALOG_NUMBER);
+    materialSampleDto.setDwcOtherCatalogNumbers(DWC_OTHER_CATALOG_NUMBERS);
+    materialSampleDto.setCollectingEvent(null);
+    materialSampleDto.setPreparedBy(ExternalRelationDto.builder().id(PREPARED_BY.toString()).type("agent").build());
+    materialSampleDto.setPreparationDate(PREPARATION_DATE);
+    materialSampleDto.setGroup(GROUP);
+    materialSampleDto.setMaterialSampleName(MATERIAL_SAMPLE_NAME);
+    materialSampleDto.setAttachment(List.of(
+        ExternalRelationDto.builder().id(UUID.randomUUID().toString()).type("metadata").build()));
+    materialSampleDto.setCreatedBy(CREATED_BY);
+    return materialSampleDto;
+  }
+}
