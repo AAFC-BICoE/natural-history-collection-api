c105de27a69bb3e78fca2d3b5869e7f3e6d80b5c
luusteve <luu.stevenluu@gmail.com>
2021-06-14 15:31:44 -0400

    create test filtering on group, preparation type

--- src/test/java/ca/gc/aafc/collection/api/repository/PreparationTypeRepositoryIT.java
+++ src/test/java/ca/gc/aafc/collection/api/repository/PreparationTypeRepositoryIT.java
@@ -5,10 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.UUID;
+import java.util.stream.Stream;
 
 import javax.inject.Inject;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.access.AccessDeniedException;
 
@@ -17,6 +21,8 @@ import ca.gc.aafc.collection.api.dto.PreparationTypeDto;
 import ca.gc.aafc.collection.api.entities.PreparationType;
 import ca.gc.aafc.collection.api.testsupport.factories.PreparationTypeFactory;
 import ca.gc.aafc.dina.testsupport.security.WithMockKeycloakUser;
+import io.crnk.core.queryspec.FilterOperator;
+import io.crnk.core.queryspec.PathSpec;
 import io.crnk.core.queryspec.QuerySpec;
 
 @SpringBootTest(properties = "keycloak.enabled=true")
@@ -53,12 +59,34 @@ public class PreparationTypeRepositoryIT extends CollectionModuleBaseIT {
       assertThrows(AccessDeniedException.class, () -> preparationTypeRepository.save(retrievedPreparationType));
   }
 
+  @ParameterizedTest
+  @MethodSource("equalFilterSource")
+  @WithMockKeycloakUser(username = "test user", groupRole = {"aafc: staff"})
+  void findAll_GroupFilteredCorrectly(String input, int expectedSize) {
+    PreparationTypeDto preparationType = newPreparationTypeDto();
+    preparationTypeRepository.create(preparationType);
+    assertEquals(expectedSize, preparationTypeRepository.findAll(newRsqlQuerySpec(input)).size());
+  }
+
+  private static Stream<Arguments> equalFilterSource() {
+    return Stream.of(
+      Arguments.of("group==aafc", 1),
+      Arguments.of("group==notAAFC", 0)
+    );
+  }
+
   private PreparationTypeDto newPreparationTypeDto() {
     PreparationTypeDto pt = new PreparationTypeDto();
     pt.setName(name);
     pt.setGroup(group);
     pt.setUuid(UUID.randomUUID());
     return pt;
+  }
+
+  private static QuerySpec newRsqlQuerySpec(String rsql) {
+    QuerySpec spec = new QuerySpec(PreparationTypeDto.class);
+    spec.addFilter(PathSpec.of("rsql").filter(FilterOperator.EQ, rsql));
+    return spec;
   }
   
 }
