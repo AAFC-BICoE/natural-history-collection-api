02865f4d38caf060798c652eb5b281e90d592ffc
Jkeyuk <jonkero2@gmail.com>
2021-06-18 09:14:15 -0400

    22937 add storage unit
    adjust service to OneToMany service

--- src/main/java/ca/gc/aafc/collection/api/service/StorageUnitService.java
+++ src/main/java/ca/gc/aafc/collection/api/service/StorageUnitService.java
@@ -2,103 +2,32 @@ package ca.gc.aafc.collection.api.service;
 
 import ca.gc.aafc.collection.api.entities.StorageUnit;
 import ca.gc.aafc.dina.jpa.BaseDAO;
-import ca.gc.aafc.dina.service.DefaultDinaService;
+import ca.gc.aafc.dina.jpa.OneToManyDinaService;
+import ca.gc.aafc.dina.jpa.OneToManyFieldHandler;
 import lombok.NonNull;
-import org.apache.commons.collections.CollectionUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.SmartValidator;
 
-import javax.persistence.criteria.Predicate;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 @Service
-public class StorageUnitService extends DefaultDinaService<StorageUnit> {
+public class StorageUnitService extends OneToManyDinaService<StorageUnit> {
 
   public StorageUnitService(@NonNull BaseDAO baseDAO, @NonNull SmartValidator sv) {
-    super(baseDAO, sv);
+    super(baseDAO, sv, List.of(
+      new OneToManyFieldHandler<>(
+        StorageUnit.class,
+        storageUnit -> storageUnit::setParentStorageUnit,
+        StorageUnit::getStorageUnitChildren,
+        "parentStorageUnit",
+        storageUnit -> storageUnit.setParentStorageUnit(null))
+    ));
   }
 
   @Override
   protected void preCreate(StorageUnit entity) {
     entity.setUuid(UUID.randomUUID());
-    linkParentAssociation(entity);
-    linkChildAssociations(entity);
-  }
-
-  @Override
-  protected void preUpdate(StorageUnit entity) {
-    linkParentAssociation(entity);
-    resolveChildAssociations(entity);
-  }
-
-  @Override
-  protected void preDelete(StorageUnit entity) {
-    unlinkParent(entity);
-    unlinkChildren(entity);
-  }
-
-  private void resolveChildAssociations(StorageUnit entity) {
-    List<StorageUnit> incomingChildren =
-      entity.getStorageUnitChildren() == null ? new ArrayList<>() : entity.getStorageUnitChildren();
-
-    Map<UUID, StorageUnit> incoming = incomingChildren.stream()
-      .collect(Collectors.toMap(StorageUnit::getUuid, Function.identity()));
-    Map<UUID, StorageUnit> currentChildren = findStorageUnitsWithParent(entity);
-
-    currentChildren.forEach((uuid, storageUnit) -> {
-      if (!incoming.containsKey(uuid)) {
-        storageUnit.setParentStorageUnit(null);
-      }
-    });
-    linkChildAssociations(entity);
-  }
-
-  private Map<UUID, StorageUnit> findStorageUnitsWithParent(StorageUnit parent) {
-    return this.findAll(
-      StorageUnit.class,
-      (criteriaBuilder, storageUnitRoot) -> new Predicate[]{
-        criteriaBuilder.equal(storageUnitRoot.get("parentStorageUnit"), parent)
-      }, null, 0, Integer.MAX_VALUE
-    ).stream().collect(Collectors.toMap(StorageUnit::getUuid, Function.identity()));
-  }
-
-  private void unlinkChildren(StorageUnit entity) {
-    if (CollectionUtils.isNotEmpty(entity.getStorageUnitChildren())) {
-      entity.getStorageUnitChildren().forEach(c -> c.setParentStorageUnit(null));
-    }
-  }
-
-  private void unlinkParent(StorageUnit entity) {
-    StorageUnit parentStorageUnit = entity.getParentStorageUnit();
-    if (parentStorageUnit != null && CollectionUtils.isNotEmpty(parentStorageUnit.getStorageUnitChildren())) {
-      parentStorageUnit.getStorageUnitChildren().removeIf(c -> entity.getUuid().equals(c.getUuid()));
-    }
-  }
-
-  private void linkChildAssociations(StorageUnit entity) {
-    if (CollectionUtils.isNotEmpty(entity.getStorageUnitChildren())) {
-      entity.getStorageUnitChildren().forEach(c -> c.setParentStorageUnit(entity));
-    }
-  }
-
-  private void linkParentAssociation(StorageUnit entity) {
-    if (entity.getParentStorageUnit() == null) {
-      return;
-    }
-
-    StorageUnit parent = entity.getParentStorageUnit();
-    if (parent.getStorageUnitChildren() == null) {
-      parent.setStorageUnitChildren(new ArrayList<>());
-    }
-
-    if (parent.getStorageUnitChildren().stream().noneMatch(c -> c.getUuid().equals(entity.getUuid()))) {
-      parent.getStorageUnitChildren().add(entity);
-    }
   }
 
 }
--- src/test/java/ca/gc/aafc/collection/api/entities/StorageUnitCRUDIT.java
+++ src/test/java/ca/gc/aafc/collection/api/entities/StorageUnitCRUDIT.java
@@ -2,14 +2,11 @@ package ca.gc.aafc.collection.api.entities;
 
 import ca.gc.aafc.collection.api.CollectionModuleBaseIT;
 import ca.gc.aafc.collection.api.service.StorageUnitService;
-import org.apache.commons.collections.CollectionUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.shaded.org.apache.commons.lang.RandomStringUtils;
 
 import javax.inject.Inject;
-import java.util.ArrayList;
-import java.util.List;
 
 class StorageUnitCRUDIT extends CollectionModuleBaseIT {
 
@@ -26,142 +23,6 @@ class StorageUnitCRUDIT extends CollectionModuleBaseIT {
     Assertions.assertNotNull(result.getCreatedOn());
     Assertions.assertEquals(unit.getName(), result.getName());
     Assertions.assertEquals(unit.getGroup(), result.getGroup());
-  }
-
-  @Test
-  void create_linkParentThroughChild() {
-    StorageUnit parent = newUnit();
-    storageUnitService.create(parent);
-
-    StorageUnit child = newUnit();
-    child.setParentStorageUnit(parent);
-    storageUnitService.create(child);
-
-    StorageUnit result = storageUnitService.findOne(child.getUuid(), StorageUnit.class);
-    Assertions.assertEquals(parent.getUuid(), result.getParentStorageUnit().getUuid());
-
-    StorageUnit parentResult = storageUnitService.findOne(parent.getUuid(), StorageUnit.class);
-    Assertions.assertNotNull(parentResult.getStorageUnitChildren());
-    Assertions.assertEquals(1, parentResult.getStorageUnitChildren().size());
-    Assertions.assertEquals(child.getUuid(), parentResult.getStorageUnitChildren().get(0).getUuid());
-  }
-
-  @Test
-  void create_linkParentThroughParent() {
-    StorageUnit child = newUnit();
-    storageUnitService.create(child);
-
-    StorageUnit parent = newUnit();
-    parent.setStorageUnitChildren(List.of(storageUnitService.findOne(child.getUuid(), StorageUnit.class)));
-    storageUnitService.create(parent);
-
-    StorageUnit result = storageUnitService.findOne(child.getUuid(), StorageUnit.class);
-    Assertions.assertEquals(parent.getUuid(), result.getParentStorageUnit().getUuid());
-
-    StorageUnit parentResult = storageUnitService.findOne(parent.getUuid(), StorageUnit.class);
-    Assertions.assertNotNull(parentResult.getStorageUnitChildren());
-    Assertions.assertEquals(1, parentResult.getStorageUnitChildren().size());
-    Assertions.assertEquals(child.getUuid(), parentResult.getStorageUnitChildren().get(0).getUuid());
-  }
-
-  @Test
-  void update_linkParentThroughChild() {
-    StorageUnit parent = newUnit();
-    storageUnitService.create(parent);
-    StorageUnit child = newUnit();
-    storageUnitService.create(child);
-
-    StorageUnit toUpdate = storageUnitService.findOne(child.getUuid(), StorageUnit.class);
-    toUpdate.setParentStorageUnit(parent);
-    storageUnitService.update(toUpdate);
-
-    StorageUnit result = storageUnitService.findOne(toUpdate.getUuid(), StorageUnit.class);
-    Assertions.assertEquals(parent.getUuid(), result.getParentStorageUnit().getUuid());
-
-    StorageUnit parentResult = storageUnitService.findOne(parent.getUuid(), StorageUnit.class);
-    Assertions.assertNotNull(parentResult.getStorageUnitChildren());
-    Assertions.assertEquals(1, parentResult.getStorageUnitChildren().size());
-    Assertions.assertEquals(child.getUuid(), parentResult.getStorageUnitChildren().get(0).getUuid());
-  }
-
-  @Test
-  void update_linkParentThroughParent() {
-    StorageUnit parent = newUnit();
-    storageUnitService.create(parent);
-    StorageUnit child = newUnit();
-    storageUnitService.create(child);
-
-    StorageUnit toUpdate = storageUnitService.findOne(parent.getUuid(), StorageUnit.class);
-    toUpdate.setStorageUnitChildren(new ArrayList<>());
-    toUpdate.getStorageUnitChildren().add(storageUnitService.findOne(child.getUuid(), StorageUnit.class));
-    storageUnitService.update(toUpdate);
-
-    StorageUnit result = storageUnitService.findOne(child.getUuid(), StorageUnit.class);
-    Assertions.assertEquals(parent.getUuid(), result.getParentStorageUnit().getUuid());
-
-    StorageUnit parentResult = storageUnitService.findOne(parent.getUuid(), StorageUnit.class);
-    Assertions.assertNotNull(parentResult.getStorageUnitChildren());
-    Assertions.assertEquals(1, parentResult.getStorageUnitChildren().size());
-    Assertions.assertEquals(child.getUuid(), parentResult.getStorageUnitChildren().get(0).getUuid());
-  }
-
-  @Test
-  void update_AddAndRemoveChildren() {
-    StorageUnit child = newUnit();
-    storageUnitService.create(child);
-    StorageUnit parent = newUnit();
-    parent.setStorageUnitChildren(List.of(storageUnitService.findOne(child.getUuid(), StorageUnit.class)));
-    storageUnitService.create(parent);
-    StorageUnit newChild = newUnit();
-    storageUnitService.create(newChild);
-
-    StorageUnit toUpdate = storageUnitService.findOne(parent.getUuid(), StorageUnit.class);
-    Assertions.assertEquals(child.getUuid(), toUpdate.getStorageUnitChildren().get(0).getUuid());
-    toUpdate.setStorageUnitChildren(new ArrayList<>());
-    toUpdate.getStorageUnitChildren().add(newChild);
-    storageUnitService.update(toUpdate);
-
-    StorageUnit parentResult = storageUnitService.findOne(parent.getUuid(), StorageUnit.class);
-    Assertions.assertNull(
-      storageUnitService.findOne(child.getUuid(), StorageUnit.class).getParentStorageUnit());
-    Assertions.assertNotNull(
-      storageUnitService.findOne(newChild.getUuid(), StorageUnit.class).getParentStorageUnit());
-    Assertions.assertEquals(1, parentResult.getStorageUnitChildren().size());
-    Assertions.assertEquals(newChild.getUuid(), parentResult.getStorageUnitChildren().get(0).getUuid());
-  }
-
-  @Test
-  void deleteChild() {
-    StorageUnit parent = newUnit();
-    storageUnitService.create(parent);
-    StorageUnit child = newUnit();
-    child.setParentStorageUnit(parent);
-    storageUnitService.create(child);
-
-    Assertions.assertEquals(
-      parent.getUuid(),
-      storageUnitService.findOne(child.getUuid(), StorageUnit.class).getParentStorageUnit().getUuid());
-
-    storageUnitService.delete(child);
-    Assertions.assertTrue(CollectionUtils.isEmpty(
-      storageUnitService.findOne(parent.getUuid(), StorageUnit.class).getStorageUnitChildren()));
-  }
-
-  @Test
-  void deleteParent() {
-    StorageUnit parent = newUnit();
-    storageUnitService.create(parent);
-    StorageUnit child = newUnit();
-    child.setParentStorageUnit(parent);
-    storageUnitService.create(child);
-
-    Assertions.assertEquals(
-      parent.getUuid(),
-      storageUnitService.findOne(child.getUuid(), StorageUnit.class).getParentStorageUnit().getUuid());
-
-    storageUnitService.delete(parent);
-    Assertions.assertNull(
-      storageUnitService.findOne(child.getUuid(), StorageUnit.class).getParentStorageUnit());
   }
 
   private StorageUnit newUnit() {
