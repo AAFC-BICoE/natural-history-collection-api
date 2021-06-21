f21b12edb8b865d5aa43ec4fa35ab0291e080e64
luusteve <45112541+luusteve@users.noreply.github.com>
2021-06-21 07:55:29 -0400

    22938_validate_managed_attribute_values (#128)
    * added ManagedAttributeValueValidator
    * CollectionManagedAttributeValueValidatorTest

---
+++ src/main/java/ca/gc/aafc/collection/api/validation/CollectionManagedAttributeValueValidator.java
@@ -0,0 +1,23 @@
+package ca.gc.aafc.collection.api.validation;
+
+import javax.inject.Named;
+
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+
+import ca.gc.aafc.collection.api.entities.CollectionManagedAttribute;
+import ca.gc.aafc.dina.service.ManagedAttributeService;
+import ca.gc.aafc.dina.validation.ManagedAttributeValueValidator;
+
+import lombok.NonNull;
+
+@Component
+public class CollectionManagedAttributeValueValidator extends ManagedAttributeValueValidator<CollectionManagedAttribute> {
+
+  public CollectionManagedAttributeValueValidator(
+      @Named("validationMessageSource") MessageSource messageSource,
+      @NonNull ManagedAttributeService<CollectionManagedAttribute> dinaService) {
+    super(messageSource, dinaService);
+  }
+
+}
