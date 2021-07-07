16a17c7b9ffd9fa3d40a04f47ede06de90cabd60
Jkeyuk <jonkero2@gmail.com>
2021-07-07 08:48:54 -0400

    23326 create determination entity
    add classes based off redmine description

---
+++ src/main/java/ca/gc/aafc/collection/api/entities/Determination.java
@@ -0,0 +1,15 @@
+package ca.gc.aafc.collection.api.entities;
+
+import java.time.LocalDate;
+
+public class Determination {
+  private String typeStatus;
+  private String typeStatusEvidence;
+
+  private String determiner;
+  private LocalDate determinedOn;
+  private String qualifier;
+
+  private String scientificNameSource;
+  private String scientificNameDetails;
+}
---
+++ src/main/java/ca/gc/aafc/collection/api/entities/DeterminationDetail.java
@@ -0,0 +1,7 @@
+package ca.gc.aafc.collection.api.entities;
+
+public class DeterminationDetail {
+  private String verbatimScientificName;
+  private String verbatimAgent;
+  private String verbatimDate;
+}
