1c01c4237e45a5600bd0288a4e17075e4e6d3146
Jkeyuk <jonkero2@gmail.com>
2021-07-16 13:23:18 -0400

    23454_storage_unit_circular_reference
    add test case for circular reference

--- src/test/java/ca/gc/aafc/collection/api/entities/StorageUnitCRUDIT.java
+++ src/test/java/ca/gc/aafc/collection/api/entities/StorageUnitCRUDIT.java
@@ -2,7 +2,6 @@ package ca.gc.aafc.collection.api.entities;
 
 import ca.gc.aafc.collection.api.CollectionModuleBaseIT;
 import ca.gc.aafc.collection.api.testsupport.factories.StorageUnitFactory;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -21,4 +20,26 @@ class StorageUnitCRUDIT extends CollectionModuleBaseIT {
     Assertions.assertEquals(unit.getGroup(), result.getGroup());
   }
 
+  @Test
+  void circularReference_ThrowsException() {
+    StorageUnit a = StorageUnitFactory.newStorageUnit()
+      .build();
+    storageUnitService.create(a);
+
+    StorageUnit b = StorageUnitFactory.newStorageUnit()
+      .build();
+    b.setParentStorageUnit(a);
+    storageUnitService.create(b);
+
+    StorageUnit c = StorageUnitFactory.newStorageUnit()
+      .build();
+    c.setParentStorageUnit(b);
+    storageUnitService.create(c);
+
+    StorageUnit update_a = storageUnitService.findOne(a.getUuid(), StorageUnit.class);
+    update_a.setParentStorageUnit(b);
+
+    // A -> B -> C -> A
+    storageUnitService.update(update_a);
+  }
 }
