f412d1e7a528b77d8b1a110b0b5a6d6a46c3ac82
luusteve <45112541+luusteve@users.noreply.github.com>
2021-07-21 18:45:17 -0400

    23493 add doe vocabulary (#169)
    * degree of establishment vocabulary
    * List VocabularyConfigurationDto
    * degreeOfEstablishment key
    * Added usage with multiple vocabulary files
    * cleanup VocabularyRepositoryIT
    
    
    Co-authored-by: @cgendreau

---
+++ src/test/java/ca/gc/aafc/collection/api/VocabularyConfigurationTest.java
@@ -0,0 +1,33 @@
+package ca.gc.aafc.collection.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.inject.Inject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest(
+  classes = CollectionModuleApiLauncher.class,
+  webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+public class VocabularyConfigurationTest extends CollectionModuleBaseIT {
+
+  @Inject
+  private VocabularyConfiguration vocabularyConfiguration;
+
+  @Test
+  void getDegreeOfEstablishment() {
+    List<VocabularyConfiguration.VocabularyElement> degreeOfEstablishment = vocabularyConfiguration.getVocabulary().get("degreeOfEstablishment");
+
+    assertNotNull(degreeOfEstablishment);
+    assertEquals("native", degreeOfEstablishment.get(0).getName());
+    assertEquals("https://dwc.tdwg.org/doe/#dwcdoe_d001", degreeOfEstablishment.get(0).getTerm());
+    assertEquals("native", degreeOfEstablishment.get(0).getLabels().get("en"));
+    assertEquals("Indigène", degreeOfEstablishment.get(0).getLabels().get("fr"));
+  }
+  
+}
