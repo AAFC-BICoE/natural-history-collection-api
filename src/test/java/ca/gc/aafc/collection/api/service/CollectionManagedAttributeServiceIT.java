7e074c438f793a5bf5b1fde6d18c1520014e7f09
Jkeyuk <jonkero2@gmail.com>
2021-07-27 11:11:18 -0400

    23546_implement_collection_managed_attrib_service_delete
    Add test cases

---
+++ src/test/java/ca/gc/aafc/collection/api/service/CollectionManagedAttributeServiceIT.java
@@ -0,0 +1,70 @@
+package ca.gc.aafc.collection.api.service;
+
+import ca.gc.aafc.collection.api.CollectionModuleBaseIT;
+import ca.gc.aafc.collection.api.entities.CollectingEvent;
+import ca.gc.aafc.collection.api.entities.CollectionManagedAttribute;
+import ca.gc.aafc.collection.api.entities.MaterialSample;
+import ca.gc.aafc.dina.entity.ManagedAttribute;
+import io.crnk.core.exception.BadRequestException;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CollectionManagedAttributeServiceIT extends CollectionModuleBaseIT {
+
+  @Test
+  void delete_WhenKeyInUseByEvent_DeniesDelete() {
+    CollectionManagedAttribute attribute = newAttribute(CollectionManagedAttribute.ManagedAttributeComponent.COLLECTING_EVENT);
+    collectionManagedAttributeService.create(attribute);
+
+    CollectingEvent event = newEvent();
+    event.setManagedAttributes(new HashMap<>(Map.of(attribute.getKey(), "test value")));
+    collectingEventService.create(event);
+    Assertions.assertTrue(
+      collectingEventService.findOne(event.getUuid(), CollectingEvent.class).getManagedAttributes()
+        .containsKey(attribute.getKey()));
+
+    Assertions.assertThrows(
+      BadRequestException.class, () -> collectionManagedAttributeService.delete(attribute));
+  }
+
+  @Test
+  void delete_WhenKeyInUseBySample_DeniesDelete() {
+    CollectionManagedAttribute attribute = newAttribute(CollectionManagedAttribute.ManagedAttributeComponent.MATERIAL_SAMPLE);
+    collectionManagedAttributeService.create(attribute);
+
+    MaterialSample sample = MaterialSample.builder().group("grp").createdBy("by").build();
+    sample.setManagedAttributes(new HashMap<>(Map.of(attribute.getKey(), "test value")));
+    materialSampleService.create(sample);
+
+    Assertions.assertTrue(
+      collectingEventService.findOne(sample.getUuid(), MaterialSample.class).getManagedAttributes()
+        .containsKey(attribute.getKey()));
+
+    Assertions.assertThrows(
+      BadRequestException.class, () -> collectionManagedAttributeService.delete(attribute));
+  }
+
+  private static CollectionManagedAttribute newAttribute(CollectionManagedAttribute.ManagedAttributeComponent component) {
+    return CollectionManagedAttribute.builder()
+      .group("grp")
+      .name(RandomStringUtils.randomAlphabetic(6))
+      .managedAttributeType(ManagedAttribute.ManagedAttributeType.STRING)
+      .createdBy("CollectionManagedAttributeServiceIT")
+      .managedAttributeComponent(component)
+      .build();
+  }
+
+  private static CollectingEvent newEvent() {
+    return CollectingEvent.builder()
+      .createdBy("CollectionManagedAttributeServiceIT")
+      .group("grp")
+      .startEventDateTime(LocalDateTime.now().minusDays(1))
+      .build();
+  }
+
+}
