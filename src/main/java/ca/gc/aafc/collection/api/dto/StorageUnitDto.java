c2cb3b5fe4aba6af5480d4fa11befca5bc133fe3
Christian Gendreau <christiangendreau@gmail.com>
2021-08-04 18:16:47 -0400

    23488 Experiment with immutable StorageUnitChild Provide CustomFieldAdapter to make sure the dto is returned

--- src/main/java/ca/gc/aafc/collection/api/dto/StorageUnitDto.java
+++ src/main/java/ca/gc/aafc/collection/api/dto/StorageUnitDto.java
@@ -1,8 +1,12 @@
 package ca.gc.aafc.collection.api.dto;
 
+import ca.gc.aafc.collection.api.entities.ImmutableStorageUnit;
 import ca.gc.aafc.collection.api.entities.StorageUnit;
 import ca.gc.aafc.collection.api.service.StorageHierarchicalObject;
 import ca.gc.aafc.dina.dto.RelatedEntity;
+import ca.gc.aafc.dina.mapper.CustomFieldAdapter;
+import ca.gc.aafc.dina.mapper.DinaFieldAdapter;
+import ca.gc.aafc.dina.mapper.DinaMapper;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import io.crnk.core.resource.annotations.JsonApiId;
@@ -16,11 +20,15 @@ import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 @Data
 @RelatedEntity(StorageUnit.class)
 @SuppressFBWarnings({"EI_EXPOSE_REP", "EI_EXPOSE_REP2"})
 @JsonApiResource(type = StorageUnitDto.TYPENAME)
+@CustomFieldAdapter(adapters = { StorageUnitDto.ImmutableStorageUnitAdapter.class})
 public class StorageUnitDto {
 
   public static final String TYPENAME = "storage-unit";
@@ -40,6 +48,7 @@ public class StorageUnitDto {
   @JsonApiRelation
   private StorageUnitDto parentStorageUnit;
 
+
   private List<ImmutableStorageUnitDto> storageUnitChildren = new ArrayList<>();
 
   private List<StorageHierarchicalObject> hierarchy;
@@ -47,4 +56,49 @@ public class StorageUnitDto {
   @JsonApiRelation
   private StorageUnitTypeDto storageUnitType;
 
+  public static class ImmutableStorageUnitAdapter implements
+      DinaFieldAdapter<StorageUnitDto, StorageUnit, List<ImmutableStorageUnitDto>, List<ImmutableStorageUnit> > {
+    private final DinaMapper<ImmutableStorageUnitDto, ImmutableStorageUnit> mapper;
+
+    public ImmutableStorageUnitAdapter() {
+      mapper = new DinaMapper<>(ImmutableStorageUnitDto.class);
+    }
+
+    @Override
+    public List<ImmutableStorageUnitDto> toDTO(
+        List<ImmutableStorageUnit> immutableStorageUnits) {
+      if(immutableStorageUnits == null) {
+        return null;
+      }
+      return immutableStorageUnits.stream().map(mapper::toDto).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<ImmutableStorageUnit> toEntity(
+        List<ImmutableStorageUnitDto> immutableStorageUnitDtos) {
+      // should not be called so no-op
+      return null;
+    }
+
+    @Override
+    public Consumer<List<ImmutableStorageUnit>> entityApplyMethod(StorageUnit entityRef) {
+      // should not be called so no-op
+      return (l) -> {};
+    }
+
+    @Override
+    public Consumer<List<ImmutableStorageUnitDto>> dtoApplyMethod(StorageUnitDto dtoRef) {
+      return dtoRef::setStorageUnitChildren;
+    }
+
+    @Override
+    public Supplier<List<ImmutableStorageUnit>> entitySupplyMethod(StorageUnit entityRef) {
+      return entityRef::getStorageUnitChildren;
+    }
+
+    @Override
+    public Supplier<List<ImmutableStorageUnitDto>> dtoSupplyMethod(StorageUnitDto dtoRef) {
+      return dtoRef::getStorageUnitChildren;
+    }
+  }
 }
