f92ab73364c2f43bbd34598d55a403f728f46069
Jkeyuk <jonkero2@gmail.com>
2021-08-25 08:46:43 -0400

    24251_collection_dto_as_meta_provider
    CollectionDto extends AttributeMetaInfoProvider

--- src/main/java/ca/gc/aafc/collection/api/dto/CollectionDto.java
+++ src/main/java/ca/gc/aafc/collection/api/dto/CollectionDto.java
@@ -1,23 +1,25 @@
 package ca.gc.aafc.collection.api.dto;
 
-import java.time.OffsetDateTime;
-import java.util.UUID;
-
-import org.javers.core.metamodel.annotation.Id;
-import org.javers.core.metamodel.annotation.PropertyName;
-
 import ca.gc.aafc.collection.api.entities.Collection;
 import ca.gc.aafc.dina.dto.RelatedEntity;
+import ca.gc.aafc.dina.repository.meta.AttributeMetaInfoProvider;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import io.crnk.core.resource.annotations.JsonApiId;
 import io.crnk.core.resource.annotations.JsonApiResource;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.javers.core.metamodel.annotation.Id;
+import org.javers.core.metamodel.annotation.PropertyName;
 
-@Data
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
 @RelatedEntity(Collection.class)
 @SuppressFBWarnings({"EI_EXPOSE_REP", "EI_EXPOSE_REP2"})
 @JsonApiResource(type = CollectionDto.TYPENAME)
-public class CollectionDto {
+public class CollectionDto extends AttributeMetaInfoProvider {
 
   public static final String TYPENAME = "collection";
   
