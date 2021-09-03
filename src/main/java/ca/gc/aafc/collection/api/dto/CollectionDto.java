0f7adba52814dbebaf7013bac98204d4b26bcfd3
Jkeyuk <jonkero2@gmail.com>
2021-09-03 10:43:19 -0400

    24351_collection_inherits_user_described_entity
    @JsonInclude(JsonInclude.Include.NON_NULL)

--- src/main/java/ca/gc/aafc/collection/api/dto/CollectionDto.java
+++ src/main/java/ca/gc/aafc/collection/api/dto/CollectionDto.java
@@ -4,6 +4,7 @@ import ca.gc.aafc.collection.api.entities.Collection;
 import ca.gc.aafc.dina.dto.RelatedEntity;
 import ca.gc.aafc.dina.i18n.MultilingualDescription;
 import ca.gc.aafc.dina.repository.meta.AttributeMetaInfoProvider;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import io.crnk.core.resource.annotations.JsonApiId;
 import io.crnk.core.resource.annotations.JsonApiResource;
@@ -44,6 +45,7 @@ public class CollectionDto extends AttributeMetaInfoProvider {
 
   private String code;
 
+  @JsonInclude(JsonInclude.Include.NON_NULL)
   private MultilingualDescription multilingualDescription;
 
 }
