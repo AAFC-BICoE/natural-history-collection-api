2c1ebf1401042662821b9920a280070c0825a630
Jkeyuk <jonkero2@gmail.com>
2021-09-13 13:42:41 -0400

    Revert "23480_Issue_with_circular_dependency_in_storage_unit"
    This reverts commit bea0c3bfad2468abf3fd54dc3e51e2ece3e75ac7.

--- src/test/java/ca/gc/aafc/collection/api/rest/StorageUnitRestIT.java
+++ src/test/java/ca/gc/aafc/collection/api/rest/StorageUnitRestIT.java
@@ -1,6 +1,7 @@
 package ca.gc.aafc.collection.api.rest;
 
 import ca.gc.aafc.collection.api.CollectionModuleApiLauncher;
+import ca.gc.aafc.collection.api.dto.ImmutableStorageUnitDto;
 import ca.gc.aafc.collection.api.dto.StorageUnitDto;
 import ca.gc.aafc.collection.api.dto.StorageUnitTypeDto;
 import ca.gc.aafc.collection.api.repository.StorageUnitRepo;
@@ -20,6 +21,7 @@ import org.testcontainers.shaded.org.apache.commons.lang.RandomStringUtils;
 import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @SpringBootTest(
   classes = CollectionModuleApiLauncher.class,
@@ -42,7 +44,7 @@ public class StorageUnitRestIT extends BaseRestAssuredTest {
     String parentId = postUnit(newUnit());
     String childId = postUnit(newUnit());
     String unitTypeId = postUnitType(unitType);
-    String unitId = postUnit(unit, getRelationshipMap(parentId, unitTypeId));
+    String unitId = postUnit(unit, getRelationshipMap(parentId, unitTypeId), 201);
     sendPatchWithRelations(unit, childId, getRelationshipMap(unitId, unitTypeId));
 
     findUnit(unitId)
@@ -68,7 +70,7 @@ public class StorageUnitRestIT extends BaseRestAssuredTest {
     StorageUnitTypeDto unitType = newUnitType();
     String parentId = postUnit(parentUnit);
     String unitTypeId = postUnitType(unitType);
-    String unitId = postUnit(unit, getRelationshipMap(parentId, unitTypeId));
+    String unitId = postUnit(unit, getRelationshipMap(parentId, unitTypeId), 201);
     findUnit(unitId)
       .body("data.attributes.hierarchy", Matchers.notNullValue())
       .body("data.attributes.hierarchy[0].uuid", Matchers.is(unitId))
@@ -102,8 +104,8 @@ public class StorageUnitRestIT extends BaseRestAssuredTest {
   @Test
   void patch_CyclicParent_Returns400BadRequest() {
     String parentId = postUnit(newUnit());
-    String childId = postUnit(newUnit(), getParentStorageUnitRelationshipMap(parentId));
-    String secondChildId = postUnit(newUnit(), getParentStorageUnitRelationshipMap(childId));
+    String childId = postUnit(newUnit(), getParentStorageUnitRelationshipMap(parentId), 201);
+    String secondChildId = postUnit(newUnit(), getParentStorageUnitRelationshipMap(childId), 201);
     sendPatch(StorageUnitDto.TYPENAME, parentId,
       JsonAPITestHelper.toJsonAPIMap(
         StorageUnitDto.TYPENAME,
@@ -114,12 +116,29 @@ public class StorageUnitRestIT extends BaseRestAssuredTest {
   }
 
   @Test
+  void post_CyclicParent_Returns400BadRequest() {
+    StorageUnitDto parent = newUnit();
+    String parentId = postUnit(parent);
+    parent.setUuid(UUID.fromString(parentId));
+
+    String childId = postUnit(newUnit(), getParentStorageUnitRelationshipMap(parentId), 201);
+
+    StorageUnitDto second = newUnit();
+
+    ImmutableStorageUnitDto parentDto = new ImmutableStorageUnitDto();
+    parentDto.setUuid(parent.getUuid());
+    second.setStorageUnitChildren(List.of(parentDto));
+
+    postUnit(second, getParentStorageUnitRelationshipMap(childId), 400);
+  }
+
+  @Test
   void delete_RelationsResolved() {
     StorageUnitDto unit = newUnit();
     String parentId = postUnit(newUnit());
     String childId = postUnit(newUnit());
     String unitTypeId = postUnitType(newUnitType());
-    String unitId = postUnit(unit, getRelationshipMap(parentId, unitTypeId));
+    String unitId = postUnit(unit, getRelationshipMap(parentId, unitTypeId), 201);
     sendPatchWithRelations(unit, childId, getParentStorageUnitRelationshipMap(unitId));
     sendDelete(StorageUnitDto.TYPENAME, unitId);
     findUnit(childId)
@@ -163,18 +182,18 @@ public class StorageUnitRestIT extends BaseRestAssuredTest {
     );
   }
 
-  private String postUnit(StorageUnitDto unit, Map<String, Object> relationshipMap) {
+  private String postUnit(StorageUnitDto unit, Map<String, Object> relationshipMap, int expectedCode) {
     return sendPost(StorageUnitDto.TYPENAME, JsonAPITestHelper.toJsonAPIMap(
         StorageUnitDto.TYPENAME,
         JsonAPITestHelper.toAttributeMap(unit),
         relationshipMap,
         null),
-      201
+      expectedCode
     ).extract().body().jsonPath().getString("data.id");
   }
 
   private String postUnit(StorageUnitDto unit) {
-    return postUnit(unit, null);
+    return postUnit(unit, null, 201);
   }
 
   private String postUnitType(StorageUnitTypeDto unitType) {
