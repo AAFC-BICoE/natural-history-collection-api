038726a7088ca8b588e191f1a1177b5a67445849
luusteve <45112541+luusteve@users.noreply.github.com>
2021-10-01 11:02:00 -0400

    Remove unused import NotNull

--- src/main/java/ca/gc/aafc/collection/api/entities/Collection.java
+++ src/main/java/ca/gc/aafc/collection/api/entities/Collection.java
@@ -9,7 +9,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
