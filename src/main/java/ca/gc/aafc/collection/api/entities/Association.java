d62332559297d5501f8b668bd795a9af66cb7a36
Jkeyuk <jonkero2@gmail.com>
2021-10-13 13:17:16 -0400

    24689_bump_dina_base_67
    Adjust Association.java

--- src/main/java/ca/gc/aafc/collection/api/entities/Association.java
+++ src/main/java/ca/gc/aafc/collection/api/entities/Association.java
@@ -1,38 +1,42 @@
 package ca.gc.aafc.collection.api.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.experimental.SuperBuilder;
+import java.io.Serializable;
 
 @Entity
 @AllArgsConstructor
-@SuperBuilder
+@Builder
 @Setter
 @Getter
 @RequiredArgsConstructor
 @Table
-public class Association {
+@IdClass(Association.class)
+public class Association implements Serializable {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Integer id;
-
   @ManyToOne(fetch = FetchType.EAGER)
-  @JoinColumn(name = "associated_with_id")
-  private ImmutableMaterialSample assignedWith;
-  
+  @JoinColumn(name = "sample_id", nullable = false, updatable = false)
+  private MaterialSample sample;
+
+  @Id
+  @ManyToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "associated_with_id", nullable = false, updatable = false)
+  private MaterialSample associatedSample;
+
   @Size(max = 50)
   private String associationType;
+
 }
