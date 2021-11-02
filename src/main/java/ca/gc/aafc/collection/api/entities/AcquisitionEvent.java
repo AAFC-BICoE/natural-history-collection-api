109d70a1c1df6e5e02850bd5ac25075ea86074f6
luusteve <45112541+luusteve@users.noreply.github.com>
2021-11-02 08:56:49 -0400

    add acquisition event and test (#250)

---
+++ src/main/java/ca/gc/aafc/collection/api/entities/AcquisitionEvent.java
@@ -0,0 +1,70 @@
+package ca.gc.aafc.collection.api.entities;
+
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.Generated;
+import org.hibernate.annotations.GenerationTime;
+import org.hibernate.annotations.NaturalId;
+import org.hibernate.annotations.NaturalIdCache;
+
+import ca.gc.aafc.dina.entity.DinaEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@Builder
+@Setter
+@Getter
+@RequiredArgsConstructor
+@NaturalIdCache
+public class AcquisitionEvent implements DinaEntity {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @NaturalId
+  @NotNull
+  @Column(unique = true)
+  private UUID uuid;
+
+
+  @Column(name = "created_on", insertable = false, updatable = false)
+  @Generated(value = GenerationTime.INSERT)
+  private OffsetDateTime createdOn;
+
+  @NotBlank
+  @Size(max = 250)
+  @Column(name = "created_by", updatable = false)
+  private String createdBy;
+
+  private UUID receivedFrom;
+
+  private LocalDate receivedDate;
+
+  @Size(max = 255)
+  private String receptionRemarks;
+
+  private UUID externallyIsolatedBy;
+
+  private LocalDate externallyIsolatedOn;
+
+  @Size(max = 250)
+  private String externallyIsolationRemarks;
+
+}
