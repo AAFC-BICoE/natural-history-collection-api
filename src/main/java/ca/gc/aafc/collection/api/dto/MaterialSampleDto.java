824fab2915c98ab0cbbedbb91839f45b71c2293f
luusteve <45112541+luusteve@users.noreply.github.com>
2021-11-08 10:49:32 -0500

    25305 add link between material sample and acquisition event (#252)
    * add relationship between materialSample and acquisitionEvent
    
    * update MaterialSampleValidator
    
    * checkstyle fix
    
    * Lazy fetchand XOR operatorS
    
    * checkstyle fix
    
    * revert to xor operator
    
    * add helper function for better readability
    
    * use primitive on isMoreThanOne
    
    Co-authored-by: Christian Gendreau <cgendreau@users.noreply.github.com>

--- src/main/java/ca/gc/aafc/collection/api/dto/MaterialSampleDto.java
+++ src/main/java/ca/gc/aafc/collection/api/dto/MaterialSampleDto.java
@@ -131,4 +131,7 @@ public class MaterialSampleDto {
   private List<AssociationDto> associations = new ArrayList<>();
 
   private Boolean allowDuplicateName = false;
+
+  @JsonApiRelation
+  private AcquisitionEventDto acquisitionEvent;
 }
