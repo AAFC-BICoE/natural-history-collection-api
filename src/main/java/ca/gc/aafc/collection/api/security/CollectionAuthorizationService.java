6c0b2205a8460638a41987ee6110e2510f321e4c
luusteve <luu.stevenluu@gmail.com>
2021-11-12 11:44:14 -0500

    change getName()

--- src/main/java/ca/gc/aafc/collection/api/security/CollectionAuthorizationService.java
+++ src/main/java/ca/gc/aafc/collection/api/security/CollectionAuthorizationService.java
@@ -25,7 +25,7 @@ public class CollectionAuthorizationService extends PermissionAuthorizationServi
 
   @Override
   public String getName() {
-    return "MinimumGroupAndRoleAuthorizationService";
+    return "CollectionAuthorizationService";
   }
   
 }
