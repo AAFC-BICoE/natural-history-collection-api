c4de9bc555ec3b1fdedae37c9de5bf0a36853361
luusteve <luu.stevenluu@gmail.com>
2021-11-18 11:38:35 -0500

    Create Project entity and service

---
+++ src/main/java/ca/gc/aafc/collection/api/entities/Project.java
@@ -0,0 +1,49 @@
+package ca.gc.aafc.collection.api.entities;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.vladmihalcea.hibernate.type.array.ListArrayType;
+
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@SuperBuilder
+@Setter
+@Getter
+@RequiredArgsConstructor
+@TypeDef(
+  name = "list-array",
+  typeClass = ListArrayType.class
+)
+public class Project extends UserDescribedDinaEntity {
+  
+  @NotBlank
+  @Size(max = 50)
+  @Column(name = "_group")
+  private String group;
+
+  private LocalDate startDate;
+  private LocalDate endDate;
+
+  @Size(max = 50)
+  private String status;
+
+  @Type(type = "list-array")
+  @Column(name = "attachment", columnDefinition = "uuid[]")
+  private List<UUID> attachment = new ArrayList<>();
+
+}
