ad37e629d906b8223818b3045b3f66739622f55a
luusteve <45112541+luusteve@users.noreply.github.com>
2021-11-24 16:08:03 -0500

    24762 Add extensions to CollectingEvent (#225)
    * Progress expose extension
    * modify .yml file, for it to be read properly

---
+++ src/test/java/ca/gc/aafc/collection/api/repository/ExtensionRepositoryIT.java
@@ -0,0 +1,47 @@
+package ca.gc.aafc.collection.api.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import ca.gc.aafc.collection.api.CollectionExtensionConfiguration;
+import ca.gc.aafc.collection.api.CollectionModuleBaseIT;
+import ca.gc.aafc.collection.api.dto.ExtensionDto;
+import ca.gc.aafc.dina.extension.FieldExtensionDefinition.Extension;
+import io.crnk.core.queryspec.QuerySpec;
+
+public class ExtensionRepositoryIT extends CollectionModuleBaseIT {
+
+  @Inject
+  private ExtensionRepository extensionRepository;
+
+  @Inject
+  private CollectionExtensionConfiguration collectionExtensionConfiguration;
+
+  @Test
+  public void findAll_ExtensionConfiguration() {
+    List<ExtensionDto> listOfExtensionDtos =
+      extensionRepository.findAll(new QuerySpec(ExtensionDto.class));
+    assertEquals(2, listOfExtensionDtos.size());
+
+    List<Extension> listOfExtension = new ArrayList<>();
+    for (ExtensionDto extensionDto : listOfExtensionDtos) {
+      listOfExtension.add(extensionDto.getExtension());
+    }
+
+    MatcherAssert.assertThat(
+      listOfExtension,
+      Matchers.containsInAnyOrder(
+        collectionExtensionConfiguration.getExtension().get("mixs_soil"),
+        collectionExtensionConfiguration.getExtension().get("mixs_sediment")
+      ));
+  }
+  
+}
