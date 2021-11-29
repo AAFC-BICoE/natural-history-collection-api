e7588be6d70436b6c7b7af4b8fb586085aefc248
Christian Gendreau <christiangendreau@gmail.com>
2021-11-29 08:49:02 -0500

    25691 Can't save MaterialSample Project relationship Failing test for material-sample project relationship

--- src/test/java/ca/gc/aafc/collection/api/testsupport/factories/ProjectFactory.java
+++ src/test/java/ca/gc/aafc/collection/api/testsupport/factories/ProjectFactory.java
@@ -6,23 +6,24 @@ import ca.gc.aafc.collection.api.entities.Project;
 import ca.gc.aafc.dina.testsupport.factories.TestableEntityFactory;
 
 public class ProjectFactory implements TestableEntityFactory<Project> {
-  
-  @Override
-  public Project getEntityInstance() {
-    return newProject().build();
-  }
 
-    /**
+  /**
    * Static method that can be called to return a configured builder that can be
    * further customized to return the actual entity object, call the .build()
    * method on a builder.
    *
    * @return Pre-configured builder with all mandatory fields set
    */
-    public static Project.ProjectBuilder<?, ?> newProject() {
-      return Project
-        .builder()
+  public static Project.ProjectBuilder<?, ?> newProject() {
+    return Project.builder()
+        .group("aafc")
+        .name(TestableEntityFactory.generateRandomNameLettersOnly(7))
         .uuid(UUID.randomUUID())
         .createdBy("test user");
-    }
+  }
+
+  @Override
+  public Project getEntityInstance() {
+    return newProject().build();
+  }
 }
