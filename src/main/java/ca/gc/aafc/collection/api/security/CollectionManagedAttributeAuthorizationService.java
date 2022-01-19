955edd28e53e15b83543e3756811cbd982532eb2
Brandon Andre <andr0284@algonquinlive.com>
2022-01-19 09:27:48 -0500

    Support #26237 - Change CollectionManagedAttributeRepo DInaAuthorizationService
    - Created the new authorization service and added it to the repo.

---
+++ src/main/java/ca/gc/aafc/collection/api/security/CollectionManagedAttributeAuthorizationService.java
@@ -0,0 +1,31 @@
+package ca.gc.aafc.collection.api.security;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+
+import ca.gc.aafc.dina.security.PermissionAuthorizationService;
+
+@Service
+public class CollectionManagedAttributeAuthorizationService extends PermissionAuthorizationService {
+
+  @Override
+  @PreAuthorize("hasMinimumGroupAndRolePermissions(@currentUser, 'COLLECTION_MANAGER', #entity)")
+  public void authorizeCreate(Object entity) {
+  }
+
+  @Override
+  @PreAuthorize("hasMinimumGroupAndRolePermissions(@currentUser, 'COLLECTION_MANAGER', #entity)")
+  public void authorizeUpdate(Object entity) {
+  }
+
+  @Override
+  @PreAuthorize("hasMinimumGroupAndRolePermissions(@currentUser, 'COLLECTION_MANAGER', #entity)")
+  public void authorizeDelete(Object entity) {
+  }
+
+  @Override
+  public String getName() {
+    return "CollectionManagedAttributeAuthorizationService";
+  }
+  
+}
