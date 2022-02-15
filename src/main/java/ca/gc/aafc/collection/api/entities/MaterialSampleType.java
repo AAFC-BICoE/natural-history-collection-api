537b069cd86675778aa42f62a5a367788834e6d0
Christian Gendreau <cgendreau@users.noreply.github.com>
2022-02-15 08:41:22 -0500

    Update MaterialSampleType.java

--- src/main/java/ca/gc/aafc/collection/api/entities/MaterialSampleType.java
+++ src/main/java/ca/gc/aafc/collection/api/entities/MaterialSampleType.java
@@ -1,7 +1,6 @@
 package ca.gc.aafc.collection.api.entities;
 
 import ca.gc.aafc.dina.entity.DinaEntity;
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
