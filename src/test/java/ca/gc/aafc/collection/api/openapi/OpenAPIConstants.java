ec199f7c93fec8ad552dcbbcc1171653a17dc193
Christian Gendreau <christiangendreau@gmail.com>
2022-02-23 10:24:08 -0500

    26618 Ajust Preparation fields Added OpenAPIConstants to reduce verbosity

---
+++ src/test/java/ca/gc/aafc/collection/api/openapi/OpenAPIConstants.java
@@ -0,0 +1,28 @@
+package ca.gc.aafc.collection.api.openapi;
+
+import lombok.SneakyThrows;
+import org.apache.http.client.utils.URIBuilder;
+
+import java.net.URL;
+
+public final class OpenAPIConstants {
+
+  private static final String SCHEME = "https";
+  static final String SPEC_HOST = "raw.githubusercontent.com";
+  static final String SPEC_PATH = "DINA-Web/collection-specs/master/schema/natural-history-collection-api.yml";
+
+  public static final URL COLLECTION_API_SPECS_URL = buildOpenAPISpecsURL();
+
+  private OpenAPIConstants() {
+  }
+
+  @SneakyThrows
+  private static URL buildOpenAPISpecsURL() {
+    URIBuilder URI_BUILDER = new URIBuilder();
+    URI_BUILDER.setScheme(SCHEME);
+    URI_BUILDER.setHost(SPEC_HOST);
+    URI_BUILDER.setPath(SPEC_PATH);
+    return URI_BUILDER.build().toURL();
+  }
+
+}
