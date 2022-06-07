4d49e3e5344f04d3ebf66b459f810a1a4716b53d
Brandon Andre <andr0284@algonquinlive.com>
2022-06-07 14:04:54 -0400

    Support #27834 - Add material sample type to abstract class
    - Removed unused imports for checkstyle check

--- src/main/java/ca/gc/aafc/collection/api/entities/MaterialSample.java
+++ src/main/java/ca/gc/aafc/collection/api/entities/MaterialSample.java
@@ -14,8 +14,6 @@ import org.javers.core.metamodel.annotation.DiffIgnore;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
