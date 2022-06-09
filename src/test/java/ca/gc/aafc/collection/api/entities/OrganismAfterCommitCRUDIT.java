4dbaa6fe70fb7c27bb7ecf63d8379d32c6763fe9
Christian Gendreau <christiangendreau@gmail.com>
2022-06-09 16:23:50 -0400

    Added TransactionalMethodProvider to allow better control of test transction boundary

---
+++ src/test/java/ca/gc/aafc/collection/api/entities/OrganismAfterCommitCRUDIT.java
@@ -0,0 +1,31 @@
+package ca.gc.aafc.collection.api.entities;
+
+import ca.gc.aafc.collection.api.CollectionModuleApiLauncher;
+import ca.gc.aafc.dina.testsupport.PostgresTestContainerInitializer;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.orm.jpa.JpaSystemException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+
+import javax.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * Same as {@link OrganismCRUDIT} but for functionalities that should be tested AFTER transaction commit.
+ */
+@SpringBootTest(classes = CollectionModuleApiLauncher.class)
+@TestPropertySource(properties = "spring.config.additional-location=classpath:application-test.yml")
+@ContextConfiguration(initializers = { PostgresTestContainerInitializer.class })
+public class OrganismAfterCommitCRUDIT {
+
+  @Inject
+  private TransactionalMethodProvider transactionalMethodProvider;
+
+  @Test
+  void targetOrganism_MixedTargetOrganism_Exception() {
+    assertThrows(JpaSystemException.class, ()-> transactionalMethodProvider.runTransactionFor_TargetOrganism_MixedTargetOrganism_Exception());
+  }
+
+}
--- src/test/java/ca/gc/aafc/collection/api/entities/OrganismCRUDIT.java
+++ src/test/java/ca/gc/aafc/collection/api/entities/OrganismCRUDIT.java
@@ -1,26 +1,21 @@
 package ca.gc.aafc.collection.api.entities;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import javax.persistence.PersistenceException;
-import javax.validation.ValidationException;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 import ca.gc.aafc.collection.api.CollectionModuleBaseIT;
 import ca.gc.aafc.collection.api.testsupport.factories.CollectionManagedAttributeFactory;
 import ca.gc.aafc.collection.api.testsupport.factories.DeterminationFactory;
 import ca.gc.aafc.collection.api.testsupport.factories.MaterialSampleFactory;
 import ca.gc.aafc.collection.api.testsupport.factories.OrganismEntityFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import javax.persistence.PersistenceException;
+import javax.validation.ValidationException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class OrganismCRUDIT extends CollectionModuleBaseIT {
 
@@ -138,7 +133,7 @@ public class OrganismCRUDIT extends CollectionModuleBaseIT {
     Organism organism = OrganismEntityFactory.newOrganism()
       .determination(new ArrayList<>(List.of(determination)))
       .build();
-      
+
     assertDoesNotThrow(() -> organismService.create(organism));
 
     // Clean up
@@ -270,42 +265,6 @@ public class OrganismCRUDIT extends CollectionModuleBaseIT {
     // The material sample only gets set to the organism from the material sample service.
     materialSample.setOrganism(organisms);
     assertDoesNotThrow(() -> materialSampleService.update(materialSample));
-
-    // Clean up
-    materialSampleService.delete(materialSample);
-    organisms.forEach(organism -> {
-      organismService.delete(organism);
-    });
-  }
-
-  @Test
-  void targetOrganism_MixedTargetOrganism_Exception() {
-    List<Organism> organisms = new ArrayList<>();
-
-    MaterialSample materialSample = MaterialSampleFactory.newMaterialSample().build();
-    materialSampleService.createAndFlush(materialSample);
-
-    Determination determination = Determination.builder()
-            .isPrimary(false)
-            .isFiledAs(false)
-            .verbatimScientificName("verbatimScientificName")
-            .build();
-
-    Organism organism1 = OrganismEntityFactory.newOrganism()
-            .isTarget(true)
-            .determination(List.of(determination))
-            .build();
-    organisms.add(organismService.createAndFlush(organism1));
-
-    Organism organism2 = OrganismEntityFactory.newOrganism()
-            .isTarget(null)
-            .determination(List.of(determination))
-            .build();
-    organisms.add(organismService.createAndFlush(organism2));
-
-    // The material sample only gets set to the organism from the material sample service.
-    materialSample.setOrganism(organisms);
-    assertThrows(PersistenceException.class, () -> materialSampleService.update(materialSample));
 
     // Clean up
     materialSampleService.delete(materialSample);
