505caaa3dfdc1b70be6c8f16d9888db01132e828
Christian Gendreau <christiangendreau@gmail.com>
2022-10-13 13:31:52 -0400

    28891 Update to dina-base 0.95 Updated dina-base and relax check for Determination to accept simple html text

--- src/main/java/ca/gc/aafc/collection/api/repository/OrganismRepository.java
+++ src/main/java/ca/gc/aafc/collection/api/repository/OrganismRepository.java
@@ -3,23 +3,33 @@ package ca.gc.aafc.collection.api.repository;
 import ca.gc.aafc.collection.api.dto.OrganismDto;
 import ca.gc.aafc.collection.api.entities.Organism;
 import ca.gc.aafc.collection.api.service.OrganismService;
+import ca.gc.aafc.dina.json.JsonDocumentInspector;
 import ca.gc.aafc.dina.mapper.DinaMapper;
 import ca.gc.aafc.dina.repository.DinaRepository;
 import ca.gc.aafc.dina.repository.external.ExternalResourceProvider;
 import ca.gc.aafc.dina.security.DinaAuthenticatedUser;
 import ca.gc.aafc.dina.security.DinaAuthorizationService;
+import ca.gc.aafc.dina.security.TextHtmlSanitizer;
 import ca.gc.aafc.dina.service.AuditService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.NonNull;
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Safelist;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.stereotype.Repository;
 
+import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
 @Repository
 public class OrganismRepository extends DinaRepository<OrganismDto, Organism> {
 
+  private static final Safelist SIMPLE_TEXT = Safelist.simpleText();
+
   private final DinaAuthenticatedUser dinaAuthenticatedUser;
+  private final ObjectMapper objectMapper;
 
   public OrganismRepository(
           @NonNull OrganismService dinaService,
@@ -34,6 +44,7 @@ public class OrganismRepository extends DinaRepository<OrganismDto, Organism> {
         new DinaMapper<>(OrganismDto.class), OrganismDto.class,
         Organism.class, null, externalResourceProvider, buildProperties, objectMapper);
     this.dinaAuthenticatedUser = dinaAuthenticatedUser.orElse(null);
+    this.objectMapper = objectMapper;
   }
 
   @Override
@@ -42,6 +53,24 @@ public class OrganismRepository extends DinaRepository<OrganismDto, Organism> {
       resource.setCreatedBy(dinaAuthenticatedUser.getUsername());
     }
     return super.create(resource);
+  }
+
+  /**
+   * We override the checkMethod to use a less aggressive check since Determination can have simple text html.
+   * @param resource
+   * @param <S>
+   */
+  @Override
+  protected <S extends OrganismDto> void checkSubmittedData(S resource) {
+    Objects.requireNonNull(this.objectMapper);
+    if (!JsonDocumentInspector.testPredicateOnValues(objectMapper.convertValue(resource, IT_OM_TYPE_REF),
+            OrganismRepository::isSafeSimpleText)) {
+      throw new IllegalArgumentException("unsafe value detected in attributes");
+    }
+  }
+
+  private static boolean isSafeSimpleText(String txt) {
+    return StringUtils.isBlank(txt) || Jsoup.isValid(txt, SIMPLE_TEXT);
   }
 
 }
