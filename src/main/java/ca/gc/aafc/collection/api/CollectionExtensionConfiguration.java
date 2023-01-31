7ac2f858332773fbf5be20d7e7b869a8bf600ac2
juliadouglasf <83604305+juliadouglasf@users.noreply.github.com>
2023-01-31 16:20:32 -0500

    Add agronomy ontology class path

--- src/main/java/ca/gc/aafc/collection/api/CollectionExtensionConfiguration.java
+++ src/main/java/ca/gc/aafc/collection/api/CollectionExtensionConfiguration.java
@@ -16,6 +16,7 @@ import ca.gc.aafc.dina.property.YamlPropertyLoaderFactory;
 @PropertySource(value = "classpath:extension/cfia_ppc.yml", factory  = YamlPropertyLoaderFactory.class)
 @PropertySource(value = "classpath:extension/phac_rg.yml", factory  = YamlPropertyLoaderFactory.class)
 @PropertySource(value = "classpath:extension/phac_cl.yml", factory  = YamlPropertyLoaderFactory.class)
+@PropertySource(value = "classpath:extension/agronomy_ontology.yml", factory = YamlPropertyLoaderFactory.class)
 public class CollectionExtensionConfiguration {
 
   private final Map<String, Extension> extension;
