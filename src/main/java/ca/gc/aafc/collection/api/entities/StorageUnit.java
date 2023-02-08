9dfb1f948a14ed828c7be5460f8ca924e5e196cd
Christian Gendreau <christiangendreau@gmail.com>
2023-02-08 11:22:08 -0500

    29896 Add barcode to storage-unit Added barcode field to storage unit

--- src/main/java/ca/gc/aafc/collection/api/entities/StorageUnit.java
+++ src/main/java/ca/gc/aafc/collection/api/entities/StorageUnit.java
@@ -13,6 +13,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Transient;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Entity
