d757585df82a9b504e9e14989fea01daacbb5a71
Christian Gendreau <christiangendreau@gmail.com>
2023-03-15 14:01:17 -0400

    Fixed Checkstyle issues

--- src/main/java/ca/gc/aafc/collection/api/entities/MaterialSampleNameGeneration.java
+++ src/main/java/ca/gc/aafc/collection/api/entities/MaterialSampleNameGeneration.java
@@ -4,6 +4,7 @@ package ca.gc.aafc.collection.api.entities;
  * Not a real entity. Just providing common structures related to MaterialSampleNameGeneration.
  */
 public class MaterialSampleNameGeneration {
-  public enum IdentifierGenerationStrategy {TYPE_BASED, DIRECT_PARENT}
-  public enum CharacterType {NUMBER, LOWER_LETTER, UPPER_LETTER}
+  public enum IdentifierGenerationStrategy { TYPE_BASED, DIRECT_PARENT }
+
+  public enum CharacterType { NUMBER, LOWER_LETTER, UPPER_LETTER }
 }
