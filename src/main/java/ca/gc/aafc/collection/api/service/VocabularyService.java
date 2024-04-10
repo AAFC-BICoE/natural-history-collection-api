64832e6da2fdf9d0c3eeb60744dd6b3b12c8417e
Christian Gendreau <christiangendreau@gmail.com>
2024-04-10 11:19:27 -0400

    33423 Add vocabulary2 endpoint Added example of ReadOnlyDinaRepositoryV2 using vocabulary

---
+++ src/main/java/ca/gc/aafc/collection/api/service/VocabularyService.java
@@ -0,0 +1,43 @@
+package ca.gc.aafc.collection.api.service;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import ca.gc.aafc.collection.api.config.CollectionVocabularyConfiguration;
+import ca.gc.aafc.collection.api.dto.VocabularyDto;
+import ca.gc.aafc.dina.service.PredicateBasedReadOnlyDinaService;
+
+public class VocabularyService implements PredicateBasedReadOnlyDinaService<String, VocabularyDto> {
+
+  private final List<VocabularyDto> vocabulary;
+
+  public VocabularyService(CollectionVocabularyConfiguration collectionVocabularyConfiguration) {
+    vocabulary = collectionVocabularyConfiguration.getVocabulary()
+      .entrySet()
+      .stream()
+      .map( entry -> new VocabularyDto(entry.getKey(), entry.getValue()))
+      .collect( Collectors.toList());
+  }
+
+  @Override
+  public VocabularyDto findOne(String key) {
+    return vocabulary.stream().filter( d -> key.equals(d.getId())).findFirst().orElse(null);
+  }
+
+  @Override
+  public List<VocabularyDto> findAll(Predicate<VocabularyDto> predicate, Integer pageOffset,
+                                     Integer pageLimit) {
+
+    Stream<VocabularyDto> stream = vocabulary.stream().filter(predicate);
+
+    if(pageOffset != null) {
+      stream = stream.skip(pageOffset);
+    }
+    if(pageLimit != null) {
+      stream = stream.limit(pageLimit);
+    }
+    return stream.collect(Collectors.toList());
+  }
+}
