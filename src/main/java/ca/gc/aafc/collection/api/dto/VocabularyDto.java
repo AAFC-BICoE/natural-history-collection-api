db836aed2ae576237d989d110675a5691a1da826
Christian Gendreau <christiangendreau@gmail.com>
2024-06-28 15:17:20 -0400

    work in progress

--- src/main/java/ca/gc/aafc/collection/api/dto/VocabularyDto.java
+++ src/main/java/ca/gc/aafc/collection/api/dto/VocabularyDto.java
@@ -2,7 +2,6 @@ package ca.gc.aafc.collection.api.dto;
 
 import ca.gc.aafc.collection.api.config.CollectionVocabularyConfiguration;
 import io.crnk.core.resource.annotations.JsonApiId;
-import io.crnk.core.resource.annotations.JsonApiResource;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -12,7 +11,6 @@ import com.toedter.spring.hateoas.jsonapi.JsonApiTypeForClass;
 
 @AllArgsConstructor
 @Getter
-@JsonApiResource(type = VocabularyDto.TYPE)
 @JsonApiTypeForClass(VocabularyDto.TYPE)
 public class VocabularyDto {
   public static final String TYPE = "vocabulary";
