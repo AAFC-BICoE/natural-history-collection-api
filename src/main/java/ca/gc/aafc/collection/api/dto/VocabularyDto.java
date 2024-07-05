9d7f27b284ee535583ebf83f4ad141c3e9c4a65e
Christian Gendreau <christiangendreau@gmail.com>
2024-07-05 11:46:27 -0400

    Removed Crnk annotation

--- src/main/java/ca/gc/aafc/collection/api/dto/VocabularyDto.java
+++ src/main/java/ca/gc/aafc/collection/api/dto/VocabularyDto.java
@@ -1,13 +1,13 @@
 package ca.gc.aafc.collection.api.dto;
 
 import ca.gc.aafc.collection.api.config.CollectionVocabularyConfiguration;
-import io.crnk.core.resource.annotations.JsonApiId;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.List;
 
 import com.toedter.spring.hateoas.jsonapi.JsonApiTypeForClass;
+import com.toedter.spring.hateoas.jsonapi.JsonApiId;
 
 @AllArgsConstructor
 @Getter
