623437bad7c4f90782e7328a48c0234f7b664a6c
BrandonAndre <brandon.andre@agr.gc.ca>
2024-10-10 14:07:03 -0400

    - Added setter and NoArgsConstructor annotations. (NoArgsConstructor requires the final keyword to be removed from the fields)

--- src/main/java/ca/gc/aafc/collection/api/dto/IndexRefreshDto.java
+++ src/main/java/ca/gc/aafc/collection/api/dto/IndexRefreshDto.java
@@ -3,20 +3,24 @@ package ca.gc.aafc.collection.api.dto;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import com.toedter.spring.hateoas.jsonapi.JsonApiId;
 import com.toedter.spring.hateoas.jsonapi.JsonApiTypeForClass;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 @JsonApiTypeForClass(IndexRefreshDto.TYPE)
 public class IndexRefreshDto {
 
   public static final String TYPE = "index-refresh";
 
   @JsonApiId
-  private final UUID id;
+  private UUID id;
 
-  private final String docType;
+  private String docType;
 
 }
