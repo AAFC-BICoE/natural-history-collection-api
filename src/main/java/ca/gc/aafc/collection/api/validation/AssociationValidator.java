cc78f6db36afe57b1b34a118d374f6cce5399503
Christian Gendreau <christiangendreau@gmail.com>
2024-10-11 12:29:02 -0400

    35045 Update to dina-base 0.129 Updated validators to use new base class

--- src/main/java/ca/gc/aafc/collection/api/validation/AssociationValidator.java
+++ src/main/java/ca/gc/aafc/collection/api/validation/AssociationValidator.java
@@ -1,58 +1,47 @@
 package ca.gc.aafc.collection.api.validation;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+
 import ca.gc.aafc.collection.api.config.CollectionVocabularyConfiguration;
 import ca.gc.aafc.collection.api.config.CollectionVocabularyConfiguration.CollectionVocabularyElement;
 import ca.gc.aafc.collection.api.entities.Association;
-import lombok.NonNull;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.stereotype.Component;
-import org.springframework.validation.Errors;
-import org.springframework.validation.Validator;
+import ca.gc.aafc.dina.validation.DinaBaseValidator;
 
 import java.util.Objects;
 import java.util.Optional;
 
 @Component
-public class AssociationValidator implements Validator {
+public class AssociationValidator extends DinaBaseValidator<Association> {
 
   public static final String ASSOCIATED_WITH_SELF_ERROR_KEY = "validation.constraint.violation.association.associatedWithSelf";
   public static final String ASSOCIATION_TYPE_NOT_IN_VOCABULARY = "validation.constraint.violation.associationTypeNotInVocabulary";
   private final CollectionVocabularyConfiguration collectionVocabularyConfiguration;
-  private final MessageSource messageSource;
 
   public AssociationValidator(
-    MessageSource messageSource, 
-    CollectionVocabularyConfiguration collectionVocabularyConfiguration
-  ) {
-    this.messageSource = messageSource;
+    MessageSource messageSource,
+    CollectionVocabularyConfiguration collectionVocabularyConfiguration) {
+
+    super(Association.class, messageSource);
     this.collectionVocabularyConfiguration = collectionVocabularyConfiguration;
   }
 
   @Override
-  public boolean supports(@NonNull Class<?> clazz) {
-    return Association.class.isAssignableFrom(clazz);
+  public void validateTarget(Association target, Errors errors) {
+    validateAssociationType(target, errors);
+    validateAssociationNotSelf(target, errors);
   }
 
-  @Override
-  public void validate(@NonNull Object target, @NonNull Errors errors) {
-    if (!supports(target.getClass())) {
-      throw new IllegalArgumentException("AssociationValidator not supported for class " + target.getClass());
-    }
-    Association association = (Association) target;
-    validateAssociationType(errors, association);
-    validateAssociationNotSelf(errors, association);
-  }
-
-  private void validateAssociationNotSelf(Errors errors, Association association) {
+  private void validateAssociationNotSelf(Association association, Errors errors) {
     if (Objects.equals(association.getAssociatedSample().getUuid(), association.getSample().getUuid())) {
       String errorMessage = getMessage(ASSOCIATED_WITH_SELF_ERROR_KEY);
       errors.rejectValue("associationType", ASSOCIATED_WITH_SELF_ERROR_KEY, errorMessage);
     }
   }
 
-  private void validateAssociationType(Errors errors, Association association) {
+  private void validateAssociationType(Association association, Errors errors) {
     if (StringUtils.isNotBlank(association.getAssociationType())) {
       Optional<CollectionVocabularyElement> foundAssociationType = collectionVocabularyConfiguration.getVocabulary().get("associationType")
         .stream().filter(o -> o.getName().equalsIgnoreCase(association.getAssociationType())).findFirst();
@@ -65,7 +54,4 @@ public class AssociationValidator implements Validator {
     }
   }
 
-  private String getMessage(String key) {
-    return messageSource.getMessage(key, null, LocaleContextHolder.getLocale());
-  }
 }
--- src/main/java/ca/gc/aafc/collection/api/validation/VocabularyBasedValidator.java
+++ src/main/java/ca/gc/aafc/collection/api/validation/VocabularyBasedValidator.java
@@ -9,34 +9,18 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 import ca.gc.aafc.collection.api.config.CollectionVocabularyConfiguration;
+import ca.gc.aafc.dina.validation.DinaBaseValidator;
 
 /**
  * Based, package-protected class used to validate values against specific vocabulary.
  * @param <T>
  */
-abstract class VocabularyBasedValidator<T> implements Validator {
+abstract class VocabularyBasedValidator<T> extends DinaBaseValidator<T> {
 
   public static final String VALUE_NOT_IN_VOCABULARY = "validation.constraint.violation.notInVocabulary";
 
-  private final MessageSource messageSource;
-  private final Class<T> supportedClass;
-
   VocabularyBasedValidator(MessageSource messageSource, Class<T> supportedClass) {
-    this.messageSource = messageSource;
-    this.supportedClass = supportedClass;
-  }
-
-  @Override
-  public boolean supports(Class<?> clazz) {
-    return supportedClass.isAssignableFrom(clazz);
-  }
-
-  @Override
-  public void validate(Object target, Errors errors) {
-    if (!supports(target.getClass())) {
-      throw new IllegalArgumentException("VocabularyBasedValidator not supported for class " + target.getClass());
-    }
-    validateVocabularyBasedAttribute(supportedClass.cast(target), errors);
+    super(supportedClass, messageSource);
   }
 
   /**
@@ -67,14 +51,4 @@ abstract class VocabularyBasedValidator<T> implements Validator {
   protected Optional<CollectionVocabularyConfiguration.CollectionVocabularyElement> findInVocabulary(String value, List<CollectionVocabularyConfiguration.CollectionVocabularyElement> vocabularyElements) {
     return vocabularyElements.stream().filter(o -> o.getKey().equalsIgnoreCase(value)).findFirst();
   }
-
-  protected abstract void validateVocabularyBasedAttribute(T target, Errors errors);
-
-  protected String getMessage(String key) {
-    return messageSource.getMessage(key, null, LocaleContextHolder.getLocale());
-  }
-  protected String getMessage(String key, Object... messageArgs) {
-    return messageSource.getMessage(key, messageArgs, LocaleContextHolder.getLocale());
-  }
-
 }
