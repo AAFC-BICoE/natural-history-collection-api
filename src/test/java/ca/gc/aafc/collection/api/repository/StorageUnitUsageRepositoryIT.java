fac514d8d04731aae78ec4e7413d1d089d2f9027
Christian Gendreau <christiangendreau@gmail.com>
2024-10-28 16:01:06 -0400

    35136 Issue with StorageUnitUsage regex on wellRow Fixed regex + added test

--- src/test/java/ca/gc/aafc/collection/api/repository/StorageUnitUsageRepositoryIT.java
+++ src/test/java/ca/gc/aafc/collection/api/repository/StorageUnitUsageRepositoryIT.java
@@ -3,15 +3,23 @@ package ca.gc.aafc.collection.api.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.validation.Errors;
 
 import ca.gc.aafc.collection.api.CollectionModuleBaseIT;
 import ca.gc.aafc.collection.api.dto.StorageUnitUsageDto;
 import ca.gc.aafc.collection.api.dto.StorageUnitDto;
 import ca.gc.aafc.collection.api.dto.StorageUnitTypeDto;
+import ca.gc.aafc.collection.api.entities.StorageUnit;
+import ca.gc.aafc.collection.api.entities.StorageUnitType;
+import ca.gc.aafc.collection.api.entities.StorageUnitUsage;
+import ca.gc.aafc.collection.api.testsupport.factories.StorageUnitFactory;
+import ca.gc.aafc.collection.api.testsupport.factories.StorageUnitTypeFactory;
 import ca.gc.aafc.collection.api.testsupport.fixtures.StorageUnitUsageTestFixture;
 import ca.gc.aafc.collection.api.testsupport.fixtures.StorageUnitTestFixture;
 import ca.gc.aafc.collection.api.testsupport.fixtures.StorageUnitTypeTestFixture;
+import ca.gc.aafc.dina.entity.StorageGridLayout;
 import ca.gc.aafc.dina.testsupport.security.WithMockKeycloakUser;
+import ca.gc.aafc.dina.validation.ValidationErrorsHelper;
 
 import javax.inject.Inject;
 import javax.validation.ConstraintViolationException;
@@ -64,6 +72,26 @@ public class StorageUnitUsageRepositoryIT extends CollectionModuleBaseIT {
     StorageUnitUsageDto dto = StorageUnitUsageTestFixture.newStorageUnitUsage(storageUnitDto);
     dto.setWellColumn(200);
     assertThrows(ValidationException.class, ()-> storageUnitUsageRepository.create(dto));
+  }
+
+  @Test
+  @WithMockKeycloakUser(username = "dev", groupRole = {"aafc:user"})
+  public void storageUnitUsageRepository_onCreateLargeStorage_NoException() {
+
+    StorageUnitTypeDto storageUnitTypeDto = StorageUnitTypeTestFixture.newStorageUnitType();
+    storageUnitTypeDto.getGridLayoutDefinition().setNumberOfColumns(10);
+    storageUnitTypeDto.getGridLayoutDefinition().setNumberOfRows(50);
+    storageUnitTypeDto = storageUnitTypeRepo.create(storageUnitTypeDto);
+
+    StorageUnitDto storageUnitDto = StorageUnitTestFixture.newStorageUnit();
+    storageUnitDto.setStorageUnitType(storageUnitTypeDto);
+    storageUnitDto = storageUnitRepo.create(storageUnitDto);
+
+    StorageUnitUsageDto dto = StorageUnitUsageTestFixture.newStorageUnitUsage(storageUnitDto);
+    dto.setWellRow("AH");
+    dto.setWellColumn(1);
+
+    storageUnitUsageRepository.create(dto);
   }
 
   @Test
