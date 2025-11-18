6bb8978fc6094c03c2bfa9d9fb621ccc34412f0d
Christian Gendreau <christiangendreau@gmail.com>
2025-11-18 09:26:53 -0500

    Migrated FormTemplate resource to v2

--- src/main/java/ca/gc/aafc/collection/api/repository/FormTemplateRepository.java
+++ src/main/java/ca/gc/aafc/collection/api/repository/FormTemplateRepository.java
@@ -1,30 +1,53 @@
 package ca.gc.aafc.collection.api.repository;
 
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import ca.gc.aafc.collection.api.dto.FormTemplateDto;
 import ca.gc.aafc.collection.api.entities.FormTemplate;
+import ca.gc.aafc.collection.api.mapper.FormTemplateMapper;
 import ca.gc.aafc.collection.api.service.FormTemplateService;
-import ca.gc.aafc.dina.mapper.DinaMapper;
-import ca.gc.aafc.dina.repository.DinaRepository;
-import ca.gc.aafc.dina.repository.external.ExternalResourceProvider;
+import ca.gc.aafc.dina.exception.ResourceGoneException;
+import ca.gc.aafc.dina.exception.ResourceNotFoundException;
+import ca.gc.aafc.dina.exception.ResourcesGoneException;
+import ca.gc.aafc.dina.exception.ResourcesNotFoundException;
+import ca.gc.aafc.dina.jsonapi.JsonApiBulkResourceIdentifierDocument;
+import ca.gc.aafc.dina.jsonapi.JsonApiDocument;
+import ca.gc.aafc.dina.repository.DinaRepositoryV2;
 import ca.gc.aafc.dina.security.DinaAuthenticatedUser;
 import ca.gc.aafc.dina.security.auth.ObjectOwnerAuthorizationService;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.NonNull;
-import org.springframework.boot.info.BuildProperties;
-import org.springframework.stereotype.Repository;
+import static com.toedter.spring.hateoas.jsonapi.MediaTypes.JSON_API_VALUE;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import java.util.Optional;
+import java.util.UUID;
+import javax.servlet.http.HttpServletRequest;
+import lombok.NonNull;
 
-@Repository
-public class FormTemplateRepository extends DinaRepository<FormTemplateDto, FormTemplate> {
+@RestController
+@RequestMapping(value = "${dina.apiPrefix:}", produces = JSON_API_VALUE)
+public class FormTemplateRepository extends DinaRepositoryV2<FormTemplateDto, FormTemplate> {
 
-  private final DinaAuthenticatedUser authenticatedUser;
+  private final DinaAuthenticatedUser dinaAuthenticatedUser;
 
   public FormTemplateRepository(
     @NonNull FormTemplateService formTemplateService,
     ObjectOwnerAuthorizationService authorizationService,
-    ExternalResourceProvider externalResourceProvider,
     @NonNull BuildProperties buildProperties,
     Optional<DinaAuthenticatedUser> dinaAuthenticatedUser,
     ObjectMapper objectMapper
@@ -32,18 +55,61 @@ public class FormTemplateRepository extends DinaRepository<FormTemplateDto, Form
     super(formTemplateService,
       authorizationService,
       Optional.empty(),
-      new DinaMapper<>(FormTemplateDto.class),
+      FormTemplateMapper.INSTANCE,
       FormTemplateDto.class, FormTemplate.class,
-      null,
-      externalResourceProvider, buildProperties, objectMapper);
-    this.authenticatedUser = dinaAuthenticatedUser.orElse(null);
+      buildProperties, objectMapper);
+    this.dinaAuthenticatedUser = dinaAuthenticatedUser.orElse(null);
   }
 
+
   @Override
-  public <S extends FormTemplateDto> S create(S resource) {
-    if (authenticatedUser != null) {
-      resource.setCreatedBy(authenticatedUser.getUsername());
+  protected Link generateLinkToResource(FormTemplateDto dto) {
+    try {
+      return linkTo(methodOn(FormTemplateRepository.class).onFindOne(dto.getUuid(), null)).withSelfRel();
+    } catch (ResourceNotFoundException | ResourceGoneException e) {
+      throw new RuntimeException(e);
     }
-    return super.create(resource);
+  }
+
+  @PostMapping(path = FormTemplateDto.TYPENAME + "/" + DinaRepositoryV2.JSON_API_BULK_LOAD_PATH, consumes = JSON_API_BULK)
+  public ResponseEntity<RepresentationModel<?>> onBulkLoad(@RequestBody
+                                                           JsonApiBulkResourceIdentifierDocument jsonApiBulkDocument,
+                                                           HttpServletRequest req)
+      throws ResourcesNotFoundException, ResourcesGoneException {
+    return handleBulkLoad(jsonApiBulkDocument, req);
+  }
+
+  @GetMapping(FormTemplateDto.TYPENAME + "/{id}")
+  public ResponseEntity<RepresentationModel<?>> onFindOne(@PathVariable UUID id, HttpServletRequest req)
+    throws ResourceNotFoundException, ResourceGoneException {
+    return handleFindOne(id, req);
+  }
+
+  @GetMapping(FormTemplateDto.TYPENAME)
+  public ResponseEntity<RepresentationModel<?>> onFindAll(HttpServletRequest req) {
+    return handleFindAll(req);
+  }
+
+  @PostMapping(FormTemplateDto.TYPENAME)
+  @Transactional
+  public ResponseEntity<RepresentationModel<?>> onCreate(@RequestBody JsonApiDocument postedDocument) {
+    return handleCreate(postedDocument, dto -> {
+      if (dinaAuthenticatedUser != null) {
+        dto.setCreatedBy(dinaAuthenticatedUser.getUsername());
+      }
+    });
+  }
+
+  @PatchMapping(FormTemplateDto.TYPENAME + "/{id}")
+  @Transactional
+  public ResponseEntity<RepresentationModel<?>> onUpdate(@RequestBody JsonApiDocument partialPatchDto,
+                                                         @PathVariable UUID id) throws ResourceNotFoundException, ResourceGoneException {
+    return handleUpdate(partialPatchDto, id);
+  }
+
+  @DeleteMapping(FormTemplateDto.TYPENAME + "/{id}")
+  @Transactional
+  public ResponseEntity<RepresentationModel<?>> onDelete(@PathVariable UUID id) throws ResourceNotFoundException, ResourceGoneException {
+    return handleDelete(id);
   }
 }
