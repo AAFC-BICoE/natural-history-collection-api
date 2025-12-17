16fb3a363fa3d40ab8c7a7cc3c39243ba9424920
Christian Gendreau <christiangendreau@gmail.com>
2025-12-17 13:57:20 -0500

    Moved validation to service

--- src/main/java/ca/gc/aafc/collection/api/repository/ExtensionRepository.java
+++ src/main/java/ca/gc/aafc/collection/api/repository/ExtensionRepository.java
@@ -13,7 +13,6 @@ import com.toedter.spring.hateoas.jsonapi.JsonApiModelBuilder;
 
 import ca.gc.aafc.collection.api.dto.ExtensionDto;
 import ca.gc.aafc.collection.api.service.ExtensionService;
-import ca.gc.aafc.dina.extension.FieldExtensionDefinition;
 import ca.gc.aafc.dina.repository.ReadOnlyDinaRepositoryV2;
 
 import static com.toedter.spring.hateoas.jsonapi.JsonApiModelBuilder.jsonApiModel;
@@ -22,7 +21,6 @@ import static com.toedter.spring.hateoas.jsonapi.MediaTypes.JSON_API_VALUE;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import lombok.NonNull;
 
@@ -30,32 +28,9 @@ import lombok.NonNull;
 @RequestMapping(value = "${dina.apiPrefix:}", produces = JSON_API_VALUE)
 public class ExtensionRepository extends ReadOnlyDinaRepositoryV2<String, ExtensionDto> {
 
-  //private final List<ExtensionDto> extension;
-
-  protected ExtensionRepository(
-    @NonNull ExtensionService extensionService) {
+  protected ExtensionRepository(@NonNull ExtensionService extensionService) {
     super(extensionService);
-   // checkArguments(extensionConfiguration.getExtension());
-
-//    extension = extensionConfiguration.getExtension()
-//      .entrySet()
-//      .stream()
-//      .map( entry -> new ExtensionDto(entry.getKey(), entry.getValue()))
-//      .collect( Collectors.toList());
   }
-
-//  @GetMapping(ExtensionDto.TYPENAME)
-//  public ResponseEntity<RepresentationModel<?>> handleFindAll() {
-//
-//    FieldExtensionValueDto dto = findAll()
-//    if (dto == null) {
-//      return ResponseEntity.notFound().build();
-//    }
-//
-//    JsonApiModelBuilder builder = jsonApiModel().model(RepresentationModel.of(dto));
-//
-//    return ResponseEntity.ok(builder.build());
-//  }
 
   @GetMapping(ExtensionDto.TYPENAME + "/{id}")
   public ResponseEntity<RepresentationModel<?>> handleFindOne(@PathVariable String id) {
@@ -67,7 +42,6 @@ public class ExtensionRepository extends ReadOnlyDinaRepositoryV2<String, Extens
     }
 
     JsonApiModelBuilder builder = jsonApiModel().model(RepresentationModel.of(dto));
-
     return ResponseEntity.ok(builder.build());
   }
 
@@ -85,26 +59,7 @@ public class ExtensionRepository extends ReadOnlyDinaRepositoryV2<String, Extens
     }
 
     JsonApiModelBuilder builder = jsonApiModel().model(CollectionModel.of(dtos));
-
     return ResponseEntity.ok(builder.build());
   }
 
-  /**
-   * Very common configuration issue that could lead to errors.
-   *
-   * @param extension
-   */
-  private void checkArguments(Map<String, FieldExtensionDefinition.Extension> extension) {
-    for (var entry : extension.entrySet()) {
-      if (!entry.getKey().equals(entry.getValue().getKey())) {
-        throw new IllegalStateException("Extension map key not matching extension key: "
-                + entry.getKey() + " vs " + entry.getValue().getKey());
-      }
-    }
-  }
-
-//  @Override
-//  public ResourceList<ExtensionDto> findAll(QuerySpec querySpec) {
-//    return querySpec.apply(extension);
-//  }
 }
