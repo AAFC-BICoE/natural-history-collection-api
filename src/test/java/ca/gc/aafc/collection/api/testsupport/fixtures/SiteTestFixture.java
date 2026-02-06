b5ef00441b4ac59176a80ae1e4e240c125600067
Christian Gendreau <christiangendreau@gmail.com>
2026-02-06 14:13:55 -0500

    Site name is not unique

--- src/test/java/ca/gc/aafc/collection/api/testsupport/fixtures/SiteTestFixture.java
+++ src/test/java/ca/gc/aafc/collection/api/testsupport/fixtures/SiteTestFixture.java
@@ -1,6 +1,5 @@
 package ca.gc.aafc.collection.api.testsupport.fixtures;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 import org.apache.commons.lang3.RandomStringUtils;
@@ -9,16 +8,12 @@ import ca.gc.aafc.dina.dto.ExternalRelationDto;
 
 public class SiteTestFixture {
   private static final String GROUP = "aafc";
-  private static final LocalDate START_DATE = LocalDate.of(1991, 01, 01);
-  private static final LocalDate END_DATE = LocalDate.now();
   private static final String CODE = "LTAE-M";
 
   public static SiteDto newSite() {
     SiteDto siteDto = new SiteDto();
     siteDto.setName(RandomStringUtils.randomAlphabetic(5));
     siteDto.setMultilingualDescription(MultilingualTestFixture.newMultilingualDescription());
-    siteDto.setStartDate(START_DATE);
-    siteDto.setEndDate(END_DATE);
     siteDto.setCode(CODE);
     siteDto.setGroup(GROUP);
 
