326be59e5b4177c3aae3613870810d0ff109dd40
Praveen Yalamanchli <praveen.yalamanchli@agr.gc.ca>
2026-02-26 18:06:19 -0500

    Testcases for siteGoem field

---
+++ src/test/java/ca/gc/aafc/collection/api/config/GeometryJacksonConfigurationTest.java
@@ -0,0 +1,45 @@
+package ca.gc.aafc.collection.api.config;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import javax.inject.Inject;
+import org.geolatte.geom.G2D;
+import org.geolatte.geom.Geometry;
+import org.geolatte.geom.GeometryType;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ca.gc.aafc.collection.api.CollectionModuleApiLauncher;
+import ca.gc.aafc.collection.api.CollectionModuleBaseIT;
+
+/*
+  This configuration class responsible for Jackson configuration verification test
+  Purpose of this test is 
+  Fail Fast if:
+    Jackson config changes
+    GeolatteGeomModule is removed ex: GeoLatteJacksonConfig class
+    Geometry<G2D> cannot deserialize GeoJSON
+    Introduces conflicting module
+*/
+@SpringBootTest(classes = CollectionModuleApiLauncher.class)
+public class GeometryJacksonConfigurationTest extends CollectionModuleBaseIT {
+
+  @Inject
+  private ObjectMapper objectMapper;
+
+  @Test
+  void deserializePolygonGeoJson_shouldReturnPolygon() throws Exception {
+
+    String geoJson = """
+        {
+          "type": "Polygon",
+          "coordinates": [[[100.0,0.0],[101.0,0.0],[101.0,1.0],[100.0,1.0],[100.0,0.0]]]
+        }
+        """;
+    Geometry<G2D> geometry = objectMapper.readValue(geoJson,
+        new com.fasterxml.jackson.core.type.TypeReference<Geometry<G2D>>() {
+        });
+    assertNotNull(geometry);
+    assertEquals(GeometryType.POLYGON, geometry.getGeometryType());
+  }
+}
