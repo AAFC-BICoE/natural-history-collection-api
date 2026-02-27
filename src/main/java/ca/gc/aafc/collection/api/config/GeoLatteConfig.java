affc1eccbfc96e23695f02816f494e1f4c3bc47e
Christian Gendreau <christiangendreau@gmail.com>
2026-02-27 09:42:29 -0500

    Fixed ObjectMapper module

--- src/main/java/ca/gc/aafc/collection/api/config/GeoLatteConfig.java
+++ src/main/java/ca/gc/aafc/collection/api/config/GeoLatteConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 public class GeoLatteConfig {
   @Bean
   public Jackson2ObjectMapperBuilderCustomizer customizer() {
-    return builder -> builder.modules(new GeolatteGeomModule());
+    // make sure to add the module and not replace the existing list
+    return builder -> builder.modulesToInstall(c -> c.add(new GeolatteGeomModule()));
   }
 }
