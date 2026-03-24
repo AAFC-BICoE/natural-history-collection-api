c7e2029347287ec87d4bed509b91846608991c43
Christian Gendreau <christiangendreau@gmail.com>
2026-03-24 14:51:20 -0400

    38047 LazyInitializationException when requesting storageUnitChildren as an optional field Added test coverage + fixed transaction issue

---
+++ src/test/java/ca/gc/aafc/collection/api/repository/StorageUnitRepositoryIT.java
@@ -0,0 +1,37 @@
+package ca.gc.aafc.collection.api.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import ca.gc.aafc.collection.api.dto.StorageUnitDto;
+import ca.gc.aafc.collection.api.testsupport.fixtures.StorageUnitTestFixture;
+import ca.gc.aafc.dina.testsupport.security.WithMockKeycloakUser;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.UUID;
+import javax.inject.Inject;
+
+public class StorageUnitRepositoryIT extends BaseRepositoryIT {
+
+  @Inject
+  private StorageUnitRepo storageUnitRepo;
+
+  @Test
+  @WithMockKeycloakUser(username = "dev", groupRole = {"aafc:user"})
+  void create_findUsingOptFields() {
+
+    StorageUnitDto storageUnitDto = StorageUnitTestFixture.newStorageUnit();
+    UUID storageUnitUUID = createWithRepository(storageUnitDto, storageUnitRepo::onCreate);
+
+    MockHttpServletRequest mockRequest = new MockHttpServletRequest();
+    mockRequest.setQueryString(
+      "include=storageUnitType&optfields[storage-unit]=storageUnitChildren");
+
+    // make sure to test lazy loading
+    var response = storageUnitRepo.onFindAll(mockRequest);
+
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+  }
+}
