72f5f2af8842ff1f6a62f5584d27a2125a32e712
Christian Gendreau <christiangendreau@gmail.com>
2026-07-20 10:45:21 -0400

    Fixed remaining classes

--- src/main/java/ca/gc/aafc/collection/api/dto/CitationAuthorDto.java
+++ src/main/java/ca/gc/aafc/collection/api/dto/CitationAuthorDto.java
@@ -3,12 +3,14 @@ package ca.gc.aafc.collection.api.dto;
 import jakarta.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
 import lombok.Builder;
 import lombok.Value;
 
 @Value
 @Builder
-public class CitationAuthorDto {
+public class CitationAuthorDto implements Serializable {
 
   @Size(max = 200)
   @JsonProperty("given_names")
