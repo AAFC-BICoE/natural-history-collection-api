1efbc628bd5ae2f8f4b3936f2a8553dcafec802e
Christian Gendreau <christiangendreau@gmail.com>
2026-07-22 15:00:53 -0400

    Fixed more serilizable spotbugs reports

--- src/main/java/ca/gc/aafc/collection/api/service/StorageHierarchicalObject.java
+++ src/main/java/ca/gc/aafc/collection/api/service/StorageHierarchicalObject.java
@@ -1,5 +1,6 @@
 package ca.gc.aafc.collection.api.service;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.io.Serializable;
 import java.util.UUID;
 
@@ -15,6 +16,10 @@ import lombok.Setter;
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class StorageHierarchicalObject implements Serializable {
 
+  @SuppressFBWarnings(
+    value = "SE_BAD_FIELD",
+    justification = "this field is not serialized by Hibernate. Will inherit Serializable in dina-base 0.175"
+  )
   @JsonUnwrapped
   private HierarchicalObject hierarchicalObject;
   private UUID typeUuid;
