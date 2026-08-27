703c965ab96aa5469a0daf84292521be66ad1452
Christian Gendreau <christiangendreau@gmail.com>
2026-08-27 15:43:33 -0400

    38801 Implement Dataset resource Database migration + entity class

---
+++ src/main/java/ca/gc/aafc/collection/api/entities/Dataset.java
@@ -0,0 +1,64 @@
+package ca.gc.aafc.collection.api.entities;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.Generated;
+import org.hibernate.annotations.NaturalId;
+import org.hibernate.annotations.NaturalIdCache;
+import org.hibernate.annotations.Type;
+import org.hibernate.generator.EventType;
+
+import ca.gc.aafc.dina.entity.DinaEntity;
+import ca.gc.aafc.dina.i18n.MultilingualDescription;
+import io.hypersistence.utils.hibernate.type.json.JsonType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@Builder
+@Setter
+@Getter
+@RequiredArgsConstructor
+@NaturalIdCache
+public class Dataset implements DinaEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @NaturalId
+  @NotNull
+  @Column(unique = true)
+  private UUID uuid;
+
+  @NotBlank
+  @Column(name = "_group")
+  private String group;
+
+  @Type(JsonType.class)
+  @Column(name = "multilingual_description")
+  @Valid
+  private MultilingualDescription multilingualDescription;
+
+  @Column(insertable = false, updatable = false)
+  @Generated(event = EventType.INSERT)
+  private OffsetDateTime createdOn;
+
+  @NotBlank
+  @Column(updatable = false)
+  private String createdBy;
+
+}
